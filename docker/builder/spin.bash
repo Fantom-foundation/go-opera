2783d6939e31f12a64fbb9f12dba32522207c5ff
Samuel Marks <samuelmarks@gmail.com>
2018-09-24 13:40:05 +1000

    Fixed number padding; containers no longer `--rm`

--- docker/builder/spin.bash
+++ docker/builder/spin.bash
@@ -2,9 +2,8 @@
 
 PROJECT="${PROJECT:-lachesis}"
 node_num="$1"
-printf -v node_num "%03d" "$node_num"
 ip="$2"
 container="$PROJECT$node_num"
-docker create -e node_num="$node_num" -e node_addr="$ip" --hostname "$container" --name "$container" --network "$PROJECT-net" --ip "$ip" --rm "$PROJECT"
+docker create -e node_num="$node_num" -e node_addr="$ip" --hostname "$container" --name "$container" --network "$PROJECT-net" --ip "$ip" "$PROJECT"
 #docker start "$container"
 #docker logs -f "$container"
--- docker/builder/spin_multi.bash
+++ docker/builder/spin_multi.bash
@@ -9,10 +9,13 @@ PEERS_DIR="${PEERS_DIR:-$DIR}"
 
 # [ -f "$PEERS_DIR/peers.json" ] || echo 'peers.json not found' && exit 2
 
+digits="${#n}"
+
 for ip in $(jq -rc '.[].NetAddr' "$PEERS_DIR/peers.json"); do
   ip="${ip%:*}";
-  printf '%s assigned to %s%03d\n' "$ip" "$PROJECT" "$node_num";
-  "$DIR/spin.bash" "$node_num" "$ip"
+  printf -v node_num_p "%0${digits}d" "$node_num"
+  printf '%s assigned to %s%s\n' "$ip" "$PROJECT" "$node_num_p";
+  "$DIR/spin.bash" "$node_num_p" "$ip"
   ((node_num++))
   [ "$node_num" -gt "$n" ] && exit 0
 done
