50bd0528e61e11522eda225a386a4c2628e1a0dd
Maxim Zakharov <dp.maxime@gmail.com>
2018-10-18 00:54:47 +1100

    add log stats printing (#17)
    * add log stats printing
    
    (cherry picked from commit 3ac61023ad9616e9e626df530e8fe4171d264736)
    
    * convert log stats output into JSON
    
    * make log stats output in logrus format

--- src/log/hook.go
+++ src/log/hook.go
@@ -4,6 +4,7 @@
 package lachesis_log
 
 import (
+	"fmt"
 	"runtime/debug"
 	"sync"
 	"time"
@@ -20,13 +21,28 @@ type Hook struct {
 
 // NewLocal installs a test hook for a given local logger.
 func NewLocal(logger *logrus.Logger) {
-	logger.Hooks.Add(new(Hook))
+	h := new(Hook)
+	h.startTime = time.Now()
+	logger.Hooks.Add(h)
 }
 
 func (t *Hook) Fire(e *logrus.Entry) error {
 	t.mu.Lock()
 	defer t.mu.Unlock()
 	if e.Time.Sub(t.startTime).Seconds() > 20 { // 20 seconds in nanoseconds
+		// NOTE: we can not use logrus logger here as it seems is not re-entrant
+		// and we are inside log entry processing here
+
+		// logrus message format:
+		// time="<stamp>" level="<level>" msg="LOGSTAT" debug=0  info=0 warn=5 error=0 fatal=0 panic=0
+		fmt.Printf("time=\"" +  e.Time.Format(time.RFC3339) +
+			"\" level=debug msg=\"LOGSTAT\" debug=%d info=%d warn=%d error=%d fatal=%d panic=%d\n",
+			t.stat[logrus.DebugLevel],
+			t.stat[logrus.InfoLevel],
+			t.stat[logrus.WarnLevel],
+			t.stat[logrus.ErrorLevel],
+			t.stat[logrus.FatalLevel],
+			t.stat[logrus.PanicLevel])
 		t.stat = [6]int64{} // 6 is current value of len(logrus.AllLevels)
 		// must be adjusted if changed in future in logrus
 		t.startTime = e.Time
