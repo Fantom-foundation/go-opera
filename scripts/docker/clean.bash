fa4c436ce41933f8fa3688e2cfc6eeea12c72005
Samuel Marks <samuelmarks@gmail.com>
2018-10-23 15:37:34 +1100

    Minor script fixes

--- scripts/docker/clean.bash
+++ scripts/docker/clean.bash
@@ -7,6 +7,11 @@ declare -r DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "${DIR%/*}/set_globals.bash"
 
 rm -rf "$BUILD_DIR/nodes" "$BUILD_DIR/peers.json"
+
+if [ $(pgrep -f docker) ]; then
+  exit 0
+fi
+
 containers="$(docker ps -a --no-trunc --filter name='^/'"$PROJECT" --format '{{.Names}}')"
 
 if [ ! -z "$containers" ]; then
--- scripts/multi_run.bash
+++ scripts/multi_run.bash
@@ -35,7 +35,7 @@ for ip in $(jq -rc '.[].NetAddr' "$PEERS_DIR/lachesis_data_dir/peers.json"); do
     	if [[ "$OSTYPE" == "linux-gnu" ]]; then
             sudo ifconfig "$loopback":"$node_num" "$ip" netmask 255.0.0.0 up
         elif [[ "$OSTYPE" == "darwin"* || "$OSTYPE" == "freebsd"* ]]; then
-            sudo ifconfig "$loopback" alias "$ip" netmask 255.0.0.0 up
+            sudo ifconfig "$loopback" alias "$ip" netmask 255.255.255.254 up
         fi
     else
         declare -r node_addr="$ip"
@@ -45,7 +45,7 @@ for ip in $(jq -rc '.[].NetAddr' "$PEERS_DIR/lachesis_data_dir/peers.json"); do
 done
 
 # Run multi lachesis
-GOMAXPROCS=$(("$logicalCpuCount" - 1)) ./lachesis_linux run --datadir "$BUILD_DIR/lachesis_data_dir" --store --listen="$node_addr":12000 --log=warn --heartbeat=5s -p "$node_addr":9000 --test --test_n=10000
-rc=$?
+GOMAXPROCS=$(("$logicalCpuCount" - 1)) "$BUILD_DIR/lachesis_$TARGET_OS" run --datadir "$BUILD_DIR/lachesis_data_dir" --store --listen="$node_addr":12000 --log=warn --heartbeat=5s -p "$node_addr":9000 --test --test_n=10000
+declare -i rc=$?
 rm -rf "$BUILD_DIR/lachesis_data_dir/"
-exit $rc
+exit "$rc"
