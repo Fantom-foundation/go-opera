18978a896cec283f4d810d5f8abe4a69e4410444
Samuel Marks <samuelmarks@gmail.com>
2018-10-24 13:23:25 +1100

    Versioning

--- src/version/version.go
+++ src/version/version.go
@@ -1,19 +1,22 @@
 package version
 
+import "strings"
+
 const Maj = "0"
 const Min = "3"
-const Fix = "0"
+const Fix = "3"
+
+func dashPrependAndSliceOn(condition bool, s string) string {
+	if !condition {
+		return s
+	}
+	return "-" + s[:8]
+}
 
 var (
-	// The full version string
-	Version = "0.3.2"
-
-	// GitCommit is set with --ldflags "-X main.gitCommit=$(git rev-parse HEAD)"
+	// GitCommit is set with: -ldflags "-X main.gitCommit=$(git rev-parse HEAD)"
 	GitCommit string
-)
 
-func init() {
-	if GitCommit != "" {
-		Version += "-" + GitCommit[:8]
-	}
-}
+	// The full version string
+	Version = strings.Join([]string{Maj, Min, Fix}, ".") + dashPrependAndSliceOn(GitCommit != "", GitCommit)
+)
