197c0871a6f21e111e4c482e61ba2d29407b666b
rishflab <rishflab@hotmail.com>
2018-10-30 15:00:54 +1100

    Document interfaces (#40)
    * added interface documentation
    
    * removed some mistakes from comments
    
    * removed some mistakes from comments

--- src/net/client_socket.go
+++ src/net/client_socket.go
@@ -1,10 +1,13 @@
 package net
 
+// Message handler for server messages. Concrete implementations could
+// be created using websockets, in-memory transport, UDP etc.
 type MessageListener interface {
 	OnMessage(msg []byte, err error)
 }
 
-type ClientSocket interface {
+// ClientSocket represents a client socket connection
+type ClientSocket interface {s
 	Connect(addr string, path string) error
 	AddMessageListener(listener MessageListener)
 	SendMessage(msg []byte) error
--- src/net/server_socket.go
+++ src/net/server_socket.go
@@ -4,14 +4,17 @@ import (
 	"context"
 )
 
+// ClientConnectHook provides an interface to handle when a client application connects to the server. 
 type ClientConnectHook interface {
 	OnConnect(CID string, err error)
 }
 
+// ClientMessageHook provides an interface to handle messages from the client application.
 type ClientMessageHook interface {
 	OnMessage(CID string, msg []byte, err error)
 }
 
+// ServerSocket represents a server socket connection. 
 type ServerSocket interface {
 	Listen(addr string, path string) error
 	AddConnectHook(hook ClientConnectHook)
