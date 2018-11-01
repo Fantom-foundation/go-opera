0363e932cb273d5d54a09ce08c041cd1f425a473
Maxim Zakharov <dp.maxime@gmail.com>
2018-11-01 13:58:33 +1100

    Big testnet (#46)
    * take value of n from environment if set
    
    * add -test_delay flag

--- cmd/lachesis/commands/run_lachesis_multi.go
+++ cmd/lachesis/commands/run_lachesis_multi.go
@@ -6,15 +6,26 @@ package commands
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-const n uint = 1001
-
 func runLachesis(cmd *cobra.Command, args []string) error {
 
-	var configs [n]*CLIConfig
+	var n uint = 3
+	nValue := os.Getenv("n")
+	if len(nValue) > 0 {
+		n64, err := strconv.ParseUint(nValue, 10, 64)
+		if err != nil {
+			return err
+		}
+		n = uint(n64)
+	}
+	configs := make([]*CLIConfig, n)
+	digits := len(strconv.FormatUint(uint64(n), 10))
 
 	for i := uint(0); i < n; i++ {
 
@@ -32,7 +43,7 @@ func runLachesis(cmd *cobra.Command, args []string) error {
 		configs[i].Lachesis.BindAddr = fmt.Sprintf("127.0.0.1:%d", 12000 + i + 1)
 		configs[i].Lachesis.ServiceAddr = fmt.Sprintf("127.0.0.1:%d", 8000 + i + 1)
 		configs[i].ProxyAddr = fmt.Sprintf("127.0.0.1:%d", 9000 + i + 1)
-		configs[i].Lachesis.DataDir += fmt.Sprintf("/%04d", i)
+		configs[i].Lachesis.DataDir += fmt.Sprintf("/%0*d", digits, i)
 
 		if i > 0 {
 			go runSingleLachesis(configs[i])
