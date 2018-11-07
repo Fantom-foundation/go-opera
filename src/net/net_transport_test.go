dd250be18206f257cf5876797ee05f24c2f5f021
a.guzev <aguzev@sfxdx.ru>
2018-11-08 01:46:24 +1000

    net.NetworkTransport pretty test

--- src/net/net_transport_test.go
+++ src/net/net_transport_test.go
@@ -1,251 +1,225 @@
 package net
 
 import (
-	"reflect"
 	"sync"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/andrecronje/lachesis/src/common"
 	"github.com/andrecronje/lachesis/src/poset"
 )
 
-func TestNetworkTransport_StartStop(t *testing.T) {
-	trans, err := NewTCPTransport("127.0.0.1:0", nil, 2, time.Second, common.NewTestLogger(t))
-	if err != nil {
-		t.Fatalf("err: %v", err)
-	}
-	trans.Close()
-}
+func TestNetworkTransport(t *testing.T) {
+	logger := common.NewTestLogger(t)
+	timeout := 200 * time.Millisecond
+	maxPool := 3
 
-func TestNetworkTransport_Sync(t *testing.T) {
 	// Transport 1 is consumer
-	trans1, err := NewTCPTransport("127.0.0.1:0", nil, 2, time.Second, common.NewTestLogger(t))
-	if err != nil {
-		t.Fatalf("err: %v", err)
-	}
+	trans1, err := NewTCPTransport("127.0.0.1:0", nil, 2, time.Second, logger)
+	assert.NoError(t, err)
 	defer trans1.Close()
+
 	rpcCh := trans1.Consumer()
-
-	// Make the RPC request
-	args := SyncRequest{
-		FromID: 0,
-		Known: map[int]int{
-			0: 1,
-			1: 2,
-			2: 3,
-		},
-	}
-	resp := SyncResponse{
-		FromID: 1,
-		Events: []poset.WireEvent{
-			poset.WireEvent{
-				Body: poset.WireBody{
-					Transactions:         [][]byte(nil),
-					SelfParentIndex:      1,
-					OtherParentCreatorID: 10,
-					OtherParentIndex:     0,
-					CreatorID:            9,
-				},
-			},
-		},
-		Known: map[int]int{
-			0: 5,
-			1: 5,
-			2: 6,
-		},
-	}
-
-	// Listen for a request
-	go func() {
-		select {
-		case rpc := <-rpcCh:
-			// Verify the command
-			req := rpc.Command.(*SyncRequest)
-			if !reflect.DeepEqual(req, &args) {
-				t.Fatalf("command mismatch: %#v %#v", *req, args)
-			}
-
-			rpc.Respond(&resp, nil)
-
-		case <-time.After(200 * time.Millisecond):
-			t.Fatalf("timeout")
-		}
-	}()
 
 	// Transport 2 makes outbound request
-	trans2, err := NewTCPTransport("127.0.0.1:0", nil, 2, time.Second, common.NewTestLogger(t))
-	if err != nil {
-		t.Fatalf("err: %v", err)
-	}
+	trans2, err := NewTCPTransport("127.0.0.1:0", nil, maxPool, time.Second, logger)
+	assert.NoError(t, err)
 	defer trans2.Close()
 
-	var out SyncResponse
-	if err := trans2.Sync(trans1.LocalAddr(), &args, &out); err != nil {
-		t.Fatalf("err: %v", err)
-	}
+	t.Run("Sync", func(t *testing.T) {
+		assert := assert.New(t)
 
-	// Verify the response
-	if !reflect.DeepEqual(resp, out) {
-		t.Fatalf("command mismatch: %#v %#v", resp, out)
-	}
-}
-
-func TestNetworkTransport_EagerSync(t *testing.T) {
-	// Transport 1 is consumer
-	trans1, err := NewTCPTransport("127.0.0.1:0", nil, 2, time.Second, common.NewTestLogger(t))
-	if err != nil {
-		t.Fatalf("err: %v", err)
-	}
-	defer trans1.Close()
-	rpcCh := trans1.Consumer()
-
-	// Make the RPC request
-	args := EagerSyncRequest{
-		FromID: 0,
-		Events: []poset.WireEvent{
-			poset.WireEvent{
-				Body: poset.WireBody{
-					Transactions:         [][]byte(nil),
-					SelfParentIndex:      1,
-					OtherParentCreatorID: 10,
-					OtherParentIndex:     0,
-					CreatorID:            9,
-				},
+		expectedReq := &SyncRequest{
+			FromID: 0,
+			Known: map[int]int{
+				0: 1,
+				1: 2,
+				2: 3,
 			},
-		},
-	}
-	resp := EagerSyncResponse{
-		FromID:  1,
-		Success: true,
-	}
-
-	// Listen for a request
-	go func() {
-		select {
-		case rpc := <-rpcCh:
-			// Verify the command
-			req := rpc.Command.(*EagerSyncRequest)
-			if !reflect.DeepEqual(req, &args) {
-				t.Fatalf("command mismatch: %#v %#v", *req, args)
-			}
-
-			rpc.Respond(&resp, nil)
-
-		case <-time.After(200 * time.Millisecond):
-			t.Fatalf("timeout")
 		}
-	}()
 
-	// Transport 2 makes outbound request
-	trans2, err := NewTCPTransport("127.0.0.1:0", nil, 2, time.Second, common.NewTestLogger(t))
-	if err != nil {
-		t.Fatalf("err: %v", err)
-	}
-	defer trans2.Close()
-
-	var out EagerSyncResponse
-	if err := trans2.EagerSync(trans1.LocalAddr(), &args, &out); err != nil {
-		t.Fatalf("err: %v", err)
-	}
-
-	// Verify the response
-	if !reflect.DeepEqual(resp, out) {
-		t.Fatalf("command mismatch: %#v %#v", resp, out)
-	}
-}
-
-func TestNetworkTransport_PooledConn(t *testing.T) {
-	// Transport 1 is consumer
-	trans1, err := NewTCPTransport("127.0.0.1:0", nil, 2, time.Second, common.NewTestLogger(t))
-	if err != nil {
-		t.Fatalf("err: %v", err)
-	}
-	defer trans1.Close()
-	rpcCh := trans1.Consumer()
-
-	// Make the RPC request
-	args := SyncRequest{
-		FromID: 0,
-		Known: map[int]int{
-			0: 1,
-			1: 2,
-			2: 3,
-		},
-	}
-	resp := SyncResponse{
-		FromID: 1,
-		Events: []poset.WireEvent{
-			poset.WireEvent{
-				Body: poset.WireBody{
-					Transactions:         [][]byte(nil),
-					SelfParentIndex:      1,
-					OtherParentCreatorID: 10,
-					OtherParentIndex:     0,
-					CreatorID:            9,
+		expectedResp := &SyncResponse{
+			FromID: 1,
+			Events: []poset.WireEvent{
+				poset.WireEvent{
+					Body: poset.WireBody{
+						Transactions:         [][]byte(nil),
+						SelfParentIndex:      1,
+						OtherParentCreatorID: 10,
+						OtherParentIndex:     0,
+						CreatorID:            9,
+					},
 				},
 			},
-		},
-		Known: map[int]int{
-			0: 5,
-			1: 5,
-			2: 6,
-		},
-	}
+			Known: map[int]int{
+				0: 5,
+				1: 5,
+				2: 6,
+			},
+		}
 
-	// Listen for a request
-	go func() {
-		for {
+		go func() {
 			select {
 			case rpc := <-rpcCh:
-				// Verify the command
 				req := rpc.Command.(*SyncRequest)
-				if !reflect.DeepEqual(req, &args) {
-					t.Fatalf("command mismatch: %#v %#v", *req, args)
-				}
-				rpc.Respond(&resp, nil)
+				assert.EqualValues(expectedReq, req)
+				rpc.Respond(expectedResp, nil)
+			case <-time.After(timeout):
+				assert.Fail("timeout")
+			}
+		}()
 
-			case <-time.After(200 * time.Millisecond):
-				return
+		var resp = new(SyncResponse)
+		err := trans2.Sync(trans1.LocalAddr(), expectedReq, resp)
+		if assert.NoError(err) {
+			assert.EqualValues(expectedResp, resp)
+		}
+	})
+
+	t.Run("EagerSync", func(t *testing.T) {
+		assert := assert.New(t)
+
+		expectedReq := &EagerSyncRequest{
+			FromID: 0,
+			Events: []poset.WireEvent{
+				poset.WireEvent{
+					Body: poset.WireBody{
+						Transactions:         [][]byte(nil),
+						SelfParentIndex:      1,
+						OtherParentCreatorID: 10,
+						OtherParentIndex:     0,
+						CreatorID:            9,
+					},
+				},
+			},
+		}
+
+		expectedResp := &EagerSyncResponse{
+			FromID:  1,
+			Success: true,
+		}
+
+		go func() {
+			select {
+			case rpc := <-rpcCh:
+				req := rpc.Command.(*EagerSyncRequest)
+				assert.EqualValues(expectedReq, req)
+				rpc.Respond(expectedResp, nil)
+			case <-time.After(timeout):
+				assert.Fail("timeout")
+			}
+		}()
+
+		var resp = new(EagerSyncResponse)
+		err := trans2.EagerSync(trans1.LocalAddr(), expectedReq, resp)
+		if assert.NoError(err) {
+			assert.EqualValues(expectedResp, resp)
+		}
+	})
+
+	t.Run("FastForward", func(t *testing.T) {
+		assert := assert.New(t)
+
+		expectedReq := &FastForwardRequest{
+			FromID: 0,
+		}
+
+		frame := poset.Frame{}
+		block, err := poset.NewBlockFromFrame(1, frame)
+		assert.NoError(err)
+		expectedResp := &FastForwardResponse{
+			FromID:   1,
+			Block:    block,
+			Frame:    frame,
+			Snapshot: []byte("snapshot"),
+		}
+
+		go func() {
+			select {
+			case rpc := <-rpcCh:
+				req := rpc.Command.(*FastForwardRequest)
+				assert.EqualValues(expectedReq, req)
+				rpc.Respond(expectedResp, nil)
+			case <-time.After(timeout):
+				assert.Fail("timeout")
+			}
+		}()
+
+		var resp = new(FastForwardResponse)
+		err = trans2.FastForward(trans1.LocalAddr(), expectedReq, resp)
+		if assert.NoError(err) {
+			assert.EqualValues(expectedResp, resp)
+		}
+	})
+
+	t.Run("PooledConn", func(t *testing.T) {
+		assert := assert.New(t)
+
+		expectedReq := &SyncRequest{
+			FromID: 0,
+			Known: map[int]int{
+				0: 1,
+				1: 2,
+				2: 3,
+			},
+		}
+
+		expectedResp := &SyncResponse{
+			FromID: 1,
+			Events: []poset.WireEvent{
+				poset.WireEvent{
+					Body: poset.WireBody{
+						Transactions:         [][]byte(nil),
+						SelfParentIndex:      1,
+						OtherParentCreatorID: 10,
+						OtherParentIndex:     0,
+						CreatorID:            9,
+					},
+				},
+			},
+			Known: map[int]int{
+				0: 5,
+				1: 5,
+				2: 6,
+			},
+		}
+
+		go func() {
+			for {
+				select {
+				case rpc := <-rpcCh:
+					req := rpc.Command.(*SyncRequest)
+					assert.EqualValues(expectedReq, req)
+					rpc.Respond(expectedResp, nil)
+				case <-time.After(200 * time.Millisecond):
+					return
+				}
+			}
+		}()
+
+		wg := &sync.WaitGroup{}
+
+		appendFunc := func() {
+			defer wg.Done()
+			var resp = new(SyncResponse)
+			err := trans2.Sync(trans1.LocalAddr(), expectedReq, resp)
+			if assert.NoError(err) {
+				assert.EqualValues(expectedResp, resp)
 			}
 		}
-	}()
 
-	// Transport 2 makes outbound request, 3 conn pool
-	trans2, err := NewTCPTransport("127.0.0.1:0", nil, 3, time.Second, common.NewTestLogger(t))
-	if err != nil {
-		t.Fatalf("err: %v", err)
-	}
-	defer trans2.Close()
-
-	// Create wait group
-	wg := &sync.WaitGroup{}
-	wg.Add(5)
-
-	appendFunc := func() {
-		defer wg.Done()
-		var out SyncResponse
-		if err := trans2.Sync(trans1.LocalAddr(), &args, &out); err != nil {
-			t.Fatalf("err: %v", err)
+		// Try to do parallel appends, should stress the conn pool
+		count := maxPool * 2
+		wg.Add(count)
+		for i := 0; i < count; i++ {
+			go appendFunc()
 		}
+		wg.Wait()
 
-		// Verify the response
-		if !reflect.DeepEqual(resp, out) {
-			t.Fatalf("command mismatch: %#v %#v", resp, out)
-		}
-	}
-
-	// Try to do parallel appends, should stress the conn pool
-	for i := 0; i < 5; i++ {
-		go appendFunc()
-	}
-
-	// Wait for the routines to finish
-	wg.Wait()
-
-	// Check the conn pool size
-	addr := trans1.LocalAddr()
-	if len(trans2.connPool[addr]) != 3 {
-		t.Fatalf("Expected 2 pooled conns!")
-	}
+		// Check the conn pool size
+		addr := trans1.LocalAddr()
+		assert.Equal(maxPool, len(trans2.connPool[addr]))
+	})
 }
