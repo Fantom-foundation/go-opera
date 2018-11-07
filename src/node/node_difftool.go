2a0d33ee6e77925845e48b89b5d4ccb9e1493241
a.guzev <aguzev@sfxdx.ru>
2018-11-07 19:21:47 +1000

    node interface for difftool

---
+++ src/node/node_difftool.go
@@ -0,0 +1,20 @@
+package node
+
+/*
+ * Diff tool interface implementation
+ */
+
+func (n *Node) GetCore() *Core {
+	return n.core
+}
+
+/*
+ * Node's method candidates
+ */
+
+func (n *Node) PushTx(tx []byte) {
+	n.coreLock.Lock()
+	defer n.coreLock.Unlock()
+	n.core.AddTransactions([][]byte{tx})
+	n.logger.Debugf("PushTx('%s')", tx)
+}
