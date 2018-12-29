b7663df74d3d5aa239cce8426466efcba0b31a83
a.guzev <aguzev@sfxdx.ru>
2018-12-29 15:13:30 +1000

    type poset.FlagTable for flagtables

---
+++ src/poset/flagTable.go
@@ -0,0 +1,40 @@
+package poset
+
+import (
+	"github.com/golang/protobuf/proto"
+)
+
+type FlagTable map[EventHash]int64
+
+func (ft FlagTable) Marshal() []byte {
+	body := make(map[string]int64, len(ft))
+	for k, v := range ft {
+		body[k.String()] = v
+	}
+
+	wrapper := &FlagTableWrapper{Body: body}
+	bytes, err := proto.Marshal(wrapper)
+	if err != nil {
+		panic(err)
+	}
+
+	return bytes
+}
+
+func (ft FlagTable) Unmarshal(buf []byte) error {
+	wrapper := new(FlagTableWrapper)
+	err := proto.Unmarshal(buf, wrapper)
+	if err != nil {
+		return err
+	}
+
+	for k, v := range wrapper.Body {
+		var hash EventHash
+		err = hash.Parse(k)
+		if err != nil {
+			return err
+		}
+		ft[hash] = v
+	}
+	return nil
+}
