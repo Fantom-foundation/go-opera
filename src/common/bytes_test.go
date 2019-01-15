23154f99f745b8feeb8fb1ef58731d49b17671a9
a.guzev <aguzev@sfxdx.ru>
2019-01-15 22:35:04 +1000

    src/common helpers based on 'github.com/ethereum/go-ethereum/common'

---
+++ src/common/bytes_test.go
@@ -0,0 +1,91 @@
+package common
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCopyBytes(t *testing.T) {
+	assert := assert.New(t)
+
+	data1 := []byte{1, 2, 3, 4}
+	exp1 := []byte{1, 2, 3, 4}
+	res1 := CopyBytes(data1)
+	assert.EqualValues(res1, exp1)
+}
+
+func TestLeftPadBytes(t *testing.T) {
+	assert := assert.New(t)
+
+	val1 := []byte{1, 2, 3, 4}
+	exp1 := []byte{0, 0, 0, 0, 1, 2, 3, 4}
+
+	res1 := LeftPadBytes(val1, 8)
+	res2 := LeftPadBytes(val1, 2)
+
+	assert.EqualValues(res1, exp1)
+	assert.EqualValues(res2, val1)
+}
+
+func TestRightPadBytes(t *testing.T) {
+	assert := assert.New(t)
+
+	val := []byte{1, 2, 3, 4}
+	exp := []byte{1, 2, 3, 4, 0, 0, 0, 0}
+
+	resstd := RightPadBytes(val, 8)
+	resshrt := RightPadBytes(val, 2)
+
+	assert.EqualValues(resstd, exp)
+	assert.EqualValues(resshrt, val)
+}
+
+func TestFromHex(t *testing.T) {
+	input := "0x01"
+	expected := []byte{1}
+	result := FromHex(input)
+	if !bytes.Equal(expected, result) {
+		t.Errorf("Expected %x got %x", expected, result)
+	}
+}
+
+func TestIsHex(t *testing.T) {
+	tests := []struct {
+		input string
+		ok    bool
+	}{
+		{"", true},
+		{"0", false},
+		{"00", true},
+		{"a9e67e", true},
+		{"A9E67E", true},
+		{"0xa9e67e", false},
+		{"a9e67e001", false},
+		{"0xHELLO_MY_NAME_IS_STEVEN_@#$^&*", false},
+	}
+	for _, test := range tests {
+		if ok := isHex(test.input); ok != test.ok {
+			t.Errorf("isHex(%q) = %v, want %v", test.input, ok, test.ok)
+		}
+	}
+}
+
+func TestFromHexOddLength(t *testing.T) {
+	input := "0x1"
+	expected := []byte{1}
+	result := FromHex(input)
+	if !bytes.Equal(expected, result) {
+		t.Errorf("Expected %x got %x", expected, result)
+	}
+}
+
+func TestNoPrefixShortHexOddLength(t *testing.T) {
+	input := "1"
+	expected := []byte{1}
+	result := FromHex(input)
+	if !bytes.Equal(expected, result) {
+		t.Errorf("Expected %x got %x", expected, result)
+	}
+}
---
+++ src/common/size_test.go
@@ -0,0 +1,22 @@
+package common
+
+import (
+	"testing"
+)
+
+func TestStorageSizeString(t *testing.T) {
+	tests := []struct {
+		size StorageSize
+		str  string
+	}{
+		{2381273, "2.38 mB"},
+		{2192, "2.19 kB"},
+		{12, "12.00 B"},
+	}
+
+	for _, test := range tests {
+		if test.size.String() != test.str {
+			t.Errorf("%f: got %q, want %q", float64(test.size), test.size.String(), test.str)
+		}
+	}
+}
