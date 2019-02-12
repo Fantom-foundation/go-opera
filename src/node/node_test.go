42b1e6ce5735d9669f46ca139d45da0783fd3a94
devintegral2 <devintegral2@sfxdx.ru>
2019-02-12 17:30:52 +0700

    Move tests from oldtests/node_test.go to lachesis_test.go & node_test.go. For node_test.go I leave tests which only testing node's func. Other tests (example: gossip) move to lachesis_test.go

--- src/node/node_test.go
+++ src/node/node_test.go
@@ -4,6 +4,10 @@ import (
 	"bytes"
 	"crypto/ecdsa"
 	"fmt"
+	"math/rand"
+	"os"
+	"reflect"
+	"sync"
 	"testing"
 	"time"
 
@@ -127,6 +131,172 @@ func createNode(t *testing.T, logger *logrus.Logger, config *Config,
 	go node.Run(run)
 
 	return node
+}
+
+func gossip(
+	nodes []*Node, target int64, shutdown bool, timeout time.Duration) error {
+	for _, n := range nodes {
+		node := n
+		go func() {
+			node.Run(true)
+		}()
+	}
+	err := bombardAndWait(nodes, target, timeout)
+	if err != nil {
+		return err
+	}
+	if shutdown {
+		for _, n := range nodes {
+			n.Shutdown()
+		}
+	}
+	return nil
+}
+
+func bombardAndWait(nodes []*Node, target int64, timeout time.Duration) error {
+
+	quit := make(chan struct{})
+	go func() {
+		seq := make(map[int]int)
+		for {
+			select {
+			case <-quit:
+				return
+			default:
+				n := rand.Intn(len(nodes))
+				node := nodes[n]
+				if err := submitTransaction(node, []byte(
+					fmt.Sprintf("node%d transaction %d", n, seq[n]))); err != nil {
+					panic(err)
+				}
+				seq[n] = seq[n] + 1
+				time.Sleep(3 * time.Millisecond)
+			}
+		}
+	}()
+	tag := "beginning"
+
+	// wait until all nodes have at least 'target' blocks
+	stopper := time.After(timeout)
+	for {
+		select {
+		case <-stopper:
+			return fmt.Errorf("timeout in %v", tag)
+		default:
+		}
+		time.Sleep(10 * time.Millisecond)
+		done := true
+		for _, n := range nodes {
+			ce := n.GetLastBlockIndex()
+			if ce < target {
+				done = false
+				tag = fmt.Sprintf("ce<target:%v<%v", ce, target)
+				break
+			} else {
+				// wait until the target block has retrieved a state hash from
+				// the app
+				targetBlock, _ := n.GetBlock(target)
+				if len(targetBlock.GetStateHash()) == 0 {
+					done = false
+					tag = "stateHash==0"
+					break
+				}
+			}
+		}
+		if done {
+			break
+		}
+	}
+	close(quit)
+	return nil
+}
+
+func submitTransaction(n *Node, tx []byte) error {
+	n.proxy.SubmitCh() <- tx
+	return nil
+}
+
+func recycleNodes(
+	oldNodes []*Node, logger *logrus.Logger, t *testing.T) []*Node {
+	var newNodes []*Node
+	for _, oldNode := range oldNodes {
+		newNode := recycleNode(oldNode, logger, t)
+		newNodes = append(newNodes, newNode)
+	}
+	return newNodes
+}
+
+func recycleNode(oldNode *Node, logger *logrus.Logger, t *testing.T) *Node {
+	conf := oldNode.conf
+	id := oldNode.id
+	key := oldNode.core.key
+	ps := oldNode.peerSelector.Peers()
+
+	var store poset.Store
+	var err error
+	if _, ok := oldNode.core.poset.Store.(*poset.BadgerStore); ok {
+		store, err = poset.LoadBadgerStore(
+			conf.CacheSize, oldNode.core.poset.Store.StorePath())
+		if err != nil {
+			t.Fatal(err)
+		}
+	} else {
+		store = poset.NewInmemStore(oldNode.core.participants, conf.CacheSize, nil)
+	}
+
+	backConfig := peer.NewBackendConfig()
+	network, createFu := createNetwork()
+	p := ps.ToPeerSlice()
+
+	// Create transport
+	trans := createTransport(t, logger, backConfig, p[0].NetAddr,
+		2, createFu, network.CreateListener)
+	defer transportClose(t, trans)
+
+	prox := dummy.NewInmemDummyApp(logger)
+
+	// Create & Init node
+	newNode := NewNode(conf, id, key, ps, store, trans, prox)
+	if err := newNode.Init(); err != nil {
+		t.Fatal(err)
+	}
+
+	return newNode
+}
+
+func checkGossip(nodes []*Node, fromBlock int64, t *testing.T) {
+
+	nodeBlocks := map[uint64][]poset.Block{}
+	for _, n := range nodes {
+		var blocks []poset.Block
+		lastIndex := n.core.poset.Store.LastBlockIndex()
+		for i := fromBlock; i < lastIndex; i++ {
+			block, err := n.core.poset.Store.GetBlock(i)
+			if err != nil {
+				t.Fatalf("checkGossip: %v ", err)
+			}
+			blocks = append(blocks, block)
+		}
+		nodeBlocks[n.id] = blocks
+	}
+
+	minB := len(nodeBlocks[0])
+	for k := uint64(1); k < uint64(len(nodes)); k++ {
+		if len(nodeBlocks[k]) < minB {
+			minB = len(nodeBlocks[k])
+		}
+	}
+
+	for i, block := range nodeBlocks[0][:minB] {
+		for k := uint64(1); k < uint64(len(nodes)); k++ {
+			oBlock := nodeBlocks[k][i]
+			if !reflect.DeepEqual(block.Body, oBlock.Body) {
+				t.Fatalf("check gossip: difference in block %d."+
+					" node 0: %v, node %d: %v",
+					block.Index(), block.Body, k, oBlock.Body)
+			}
+		}
+	}
 }
 
 func TestCreateAndInitNode(t *testing.T) {
@@ -554,4 +724,312 @@ func TestRequestFastForward(t *testing.T) {
 	if !bytes.Equal(hash1, hash2) {
 		t.Fatalf("expected hash %s, got %s", hexutil.Encode(hash1), hexutil.Encode(hash2))
 	}
+}
+
+func TestFastForward(t *testing.T) {
+
+	data := InitTestData(t, 4, 2)
+
+	// Create transport
+	trans1 := createTransport(t, data.Logger, data.BackConfig, data.Adds[0],
+		data.PoolSize, data.CreateFu, data.Network.CreateListener)
+	defer transportClose(t, trans1)
+
+	trans2 := createTransport(t, data.Logger, data.BackConfig, data.Adds[1],
+		data.PoolSize, data.CreateFu, data.Network.CreateListener)
+	defer transportClose(t, trans2)
+
+	trans3 := createTransport(t, data.Logger, data.BackConfig, data.Adds[2],
+		data.PoolSize, data.CreateFu, data.Network.CreateListener)
+	defer transportClose(t, trans3)
+
+	trans4 := createTransport(t, data.Logger, data.BackConfig, data.Adds[3],
+		data.PoolSize, data.CreateFu, data.Network.CreateListener)
+	defer transportClose(t, trans4)
+
+	// Create & Init node
+	node1 := createNode(t, data.Logger, data.Config, data.PeersSlice[0].ID, data.Keys[0], data.Peers, trans1, false)
+	defer node1.Shutdown()
+
+	node2 := createNode(t, data.Logger, data.Config, data.PeersSlice[1].ID, data.Keys[1], data.Peers, trans2, false)
+	defer node2.Shutdown()
+
+	node3 := createNode(t, data.Logger, data.Config, data.PeersSlice[2].ID, data.Keys[2], data.Peers, trans3, false)
+	defer node3.Shutdown()
+
+	node4 := createNode(t, data.Logger, data.Config, data.PeersSlice[3].ID, data.Keys[3], data.Peers, trans4, false)
+	defer node4.Shutdown()
+
+	nodes := []*Node{node1, node2, node3, node4}
+
+	target := int64(3)
+	err := gossip(nodes[1:], target, false, 60*time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+	time.Sleep(2 * time.Second)
+
+	err = nodes[0].fastForward()
+	if err != nil {
+		t.Fatalf("Error FastForwarding: %s", err)
+	}
+
+	lbi := nodes[0].GetLastBlockIndex()
+	if lbi < 0 {
+		t.Fatalf("LastBlockIndex is too low: %d", lbi)
+	}
+	sBlock, err := nodes[0].GetBlock(lbi)
+	if err != nil {
+		t.Fatalf("Error retrieving latest Block"+
+			" from reset hasposetraph: %v", err)
+	}
+	expectedBlock, err := nodes[1].GetBlock(lbi)
+	if err != nil {
+		t.Fatalf("Failed to retrieve block %d from node1: %v", lbi, err)
+	}
+	if !reflect.DeepEqual(sBlock.Body, expectedBlock.Body) {
+		t.Fatalf("Blocks defer")
+	}
+}
+
+// TODO: Failed
+func TestFastSync(t *testing.T) {
+	var let sync.Mutex
+	caught := false
+	logger := common.NewTestLogger(t)
+	config := TestConfig(t)
+
+	poolSize := 2
+	backConfig := peer.NewBackendConfig()
+
+	network, createFu := createNetwork()
+	keys, p, adds := initPeers(4, network)
+	ps := p.ToPeerSlice()
+
+	trans1 := createTransport(t, logger, backConfig, adds[0],
+		poolSize, createFu, network.CreateListener)
+	defer transportClose(t, trans1)
+
+	trans2 := createTransport(t, logger, backConfig, adds[1],
+		poolSize, createFu, network.CreateListener)
+	defer transportClose(t, trans2)
+
+	trans3 := createTransport(t, logger, backConfig, adds[2],
+		poolSize, createFu, network.CreateListener)
+	defer transportClose(t, trans3)
+
+	trans4 := createTransport(t, logger, backConfig, adds[3],
+		poolSize, createFu, network.CreateListener)
+	defer transportClose(t, trans4)
+
+	node1 := createNode(t, logger, config, ps[0].ID, keys[0], p, trans1, true)
+	defer node1.Shutdown()
+
+	node2 := createNode(t, logger, config, ps[1].ID, keys[1], p, trans2, true)
+	defer node2.Shutdown()
+
+	node3 := createNode(t, logger, config, ps[2].ID, keys[2], p, trans3, true)
+	defer node3.Shutdown()
+
+	node4 := createNode(t, logger, config, ps[3].ID, keys[3], p, trans4, true)
+	defer node4.Shutdown()
+
+	nodes := []*Node{node1, node2, node3, node4}
+
+	var target int64 = 10
+
+	err := gossip(nodes, target, false, 30*time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+	checkGossip(nodes, 0, t)
+
+	node4 = nodes[3]
+	node4.Shutdown()
+
+	secondTarget := target + 10
+	err = bombardAndWait(nodes[0:3], secondTarget, 30*time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+	checkGossip(nodes[0:3], 0, t)
+
+	// Can't re-run it; have to reinstantiate a new node.
+	node4 = recycleNode(node4, logger, t)
+
+	// Run parallel routine to check node4 eventually reaches CatchingUp state.
+	timeout := time.After(30 * time.Second)
+	go func() {
+		let.Lock()
+		defer let.Unlock()
+		for {
+			select {
+			case <-timeout:
+				t.Logf("Timeout waiting for node4 to enter CatchingUp state")
+				break
+			default:
+			}
+			if node4.getState() == CatchingUp {
+				caught = true
+				break
+			}
+		}
+	}()
+
+	node4.RunAsync(true)
+	defer node4.Shutdown()
+
+	nodes[3] = node4
+
+	// Gossip some more
+	thirdTarget := secondTarget + 10
+	err = bombardAndWait(nodes, thirdTarget, 25*time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	start := node4.core.poset.FirstConsensusRound
+	checkGossip(nodes, *start, t)
+	let.Lock()
+	let.Unlock()
+	if !caught {
+		t.Fatalf("Node4 didn't reach CatchingUp state")
+	}
+}
+
+// TODO: Failed
+func TestBootstrapAllNodes(t *testing.T) {
+	logger := common.NewTestLogger(t)
+
+	if err := os.RemoveAll("test_data"); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.Mkdir("test_data", os.ModeDir|0777); err != nil {
+		t.Fatal(err)
+	}
+
+	// create a first network with BadgerStore
+	// and wait till it reaches 10 consensus rounds before shutting it down
+	config := TestConfig(t)
+
+	poolSize := 2
+	backConfig := peer.NewBackendConfig()
+
+	network, createFu := createNetwork()
+	keys, p, adds := initPeers(4, network)
+	ps := p.ToPeerSlice()
+
+	trans1 := createTransport(t, logger, backConfig, adds[0],
+		poolSize, createFu, network.CreateListener)
+	defer transportClose(t, trans1)
+
+	trans2 := createTransport(t, logger, backConfig, adds[1],
+		poolSize, createFu, network.CreateListener)
+	defer transportClose(t, trans2)
+
+	trans3 := createTransport(t, logger, backConfig, adds[2],
+		poolSize, createFu, network.CreateListener)
+	defer transportClose(t, trans3)
+
+	trans4 := createTransport(t, logger, backConfig, adds[3],
+		poolSize, createFu, network.CreateListener)
+	defer transportClose(t, trans4)
+
+	node1 := createNode(t, logger, config, ps[0].ID, keys[0], p, trans1, true)
+	defer node1.Shutdown()
+
+	node2 := createNode(t, logger, config, ps[1].ID, keys[1], p, trans2, true)
+	defer node2.Shutdown()
+
+	node3 := createNode(t, logger, config, ps[2].ID, keys[2], p, trans3, true)
+	defer node3.Shutdown()
+
+	node4 := createNode(t, logger, config, ps[3].ID, keys[3], p, trans4, true)
+	defer node4.Shutdown()
+
+	nodes := []*Node{node1, node2, node3, node4}
+
+	err := gossip(nodes, 10, false, 3*time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+	checkGossip(nodes, 0, t)
+	for _, n := range nodes {
+		n.Shutdown()
+	}
+
+	// Now try to recreate a network from the databases created
+	// in the first step and advance it to 20 consensus rounds
+	newNodes := recycleNodes(nodes, logger, t)
+	err = gossip(newNodes, 20, false, 3*time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+	checkGossip(newNodes, 0, t)
+	for _, n := range nodes {
+		n.Shutdown()
+	}
+
+	// Check that both networks did not have
+	// completely different consensus events
+	checkGossip([]*Node{nodes[0], newNodes[0]}, 0, t)
+}
+
+func TestShutdown(t *testing.T) {
+	logger := common.NewTestLogger(t)
+
+	config := TestConfig(t)
+
+	poolSize := 2
+	backConfig := peer.NewBackendConfig()
+
+	network, createFu := createNetwork()
+	keys, p, adds := initPeers(4, network)
+	ps := p.ToPeerSlice()
+
+	trans1 := createTransport(t, logger, backConfig, adds[0],
+		poolSize, createFu, network.CreateListener)
+	defer transportClose(t, trans1)
+
+	trans2 := createTransport(t, logger, backConfig, adds[1],
+		poolSize, createFu, network.CreateListener)
+	defer transportClose(t, trans2)
+
+	trans3 := createTransport(t, logger, backConfig, adds[2],
+		poolSize, createFu, network.CreateListener)
+	defer transportClose(t, trans3)
+
+	trans4 := createTransport(t, logger, backConfig, adds[3],
+		poolSize, createFu, network.CreateListener)
+	defer transportClose(t, trans4)
+
+	node1 := createNode(t, logger, config, ps[0].ID, keys[0], p, trans1, false)
+	defer node1.Shutdown()
+
+	node2 := createNode(t, logger, config, ps[1].ID, keys[1], p, trans2, false)
+	defer node2.Shutdown()
+
+	node3 := createNode(t, logger, config, ps[2].ID, keys[2], p, trans3, false)
+	defer node3.Shutdown()
+
+	node4 := createNode(t, logger, config, ps[3].ID, keys[3], p, trans4, false)
+	defer node4.Shutdown()
+
+	nodes := []*Node{node1, node2, node3, node4}
+
+	nodes[0].Shutdown()
+
+	// That modification of used counters should force SmartPeerSelector
+	// to choose nodes[0] to gossip to
+	// must be changed accordingly if PeerSelector is changed
+	nodes[1].peerSelector.Peers().ByID[nodes[1].id].Used = 2
+	nodes[1].peerSelector.Peers().ByID[nodes[2].id].Used = 2
+	nodes[1].peerSelector.Peers().ByID[nodes[0].id].Used = 1
+
+	err := nodes[1].gossip(nil)
+	if err == nil {
+		t.Fatal("Expected Timeout Error")
+	}
+
+	nodes[1].Shutdown()
 }
