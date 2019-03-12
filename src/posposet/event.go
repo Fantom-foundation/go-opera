0ee8279d2dd467c6a6d9b80fa2c88e1f783df48d
a.guzev <aguzev@sfxdx.ru>
2019-03-12 22:53:15 +1000

    poset Events ordering by parent dependency

--- src/posposet/event.go
+++ src/posposet/event.go
@@ -63,3 +63,33 @@ func (ee Events) Less(i, j int) bool {
 		(a.consensusTime == b.consensusTime && a.LamportTime < b.LamportTime) ||
 		(a.consensusTime == b.consensusTime && a.LamportTime == b.LamportTime && bytes.Compare(a.Hash().Bytes(), b.Hash().Bytes()) < 0)
 }
+
+// ByParents returns events topologically ordered by parent dependency.
+// TODO: use Topological sort algorithm
+func (ee Events) ByParents() (res Events) {
+	unsorted := make(Events, len(ee))
+	exists := EventHashes{}
+	for i, e := range ee {
+		unsorted[i] = e
+		exists.Add(e.Hash())
+	}
+	ready := EventHashes{}
+	for len(unsorted) > 0 {
+	EVENTS:
+		for i, e := range unsorted {
+
+			for p := range e.Parents {
+				if exists.Contains(p) && !ready.Contains(p) {
+					continue EVENTS
+				}
+			}
+
+			res = append(res, e)
+			unsorted = append(unsorted[0:i], unsorted[i+1:]...)
+			ready.Add(e.Hash())
+			break
+		}
+	}
+
+	return
+}
--- src/posposet/event_test.go
+++ src/posposet/event_test.go
@@ -58,3 +58,30 @@ func TestEventsSort(t *testing.T) {
 		}
 	}
 }
+
+func TestEventsByParents(t *testing.T) {
+	_, events := GenEventsByNode(5, 10, 3)
+	var unordered Events
+	for _, ee := range events {
+		unordered = append(unordered, ee...)
+	}
+
+	ordered := unordered.ByParents()
+	position := make(map[EventHash]int)
+	for i, e := range ordered {
+		position[e.Hash()] = i
+	}
+
+	for i, e := range ordered {
+		for p := range e.Parents {
+			pos, ok := position[p]
+			if !ok {
+				continue
+			}
+			if pos > i {
+				t.Fatalf("parent %s is not before %s", p.String(), e.Hash().String())
+				return
+			}
+		}
+	}
+}
