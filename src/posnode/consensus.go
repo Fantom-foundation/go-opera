290224f35d89b9eb14cca46c7bb9d1353168e02b
a.guzev <aguzev@sfxdx.ru>
2019-03-21 21:49:55 +1000

    node consensus mock for tests

--- src/posnode/consensus.go
+++ src/posnode/consensus.go
@@ -1,4 +1,19 @@
 package posnode
 
+import (
+	"github.com/Fantom-foundation/go-lachesis/src/common"
+	"github.com/Fantom-foundation/go-lachesis/src/posnode/wire"
+)
+
+// Consensus is a consensus interface.
 type Consensus interface {
+
+	// PushEvent takes event for processing.
+	PushEvent(e *wire.Event)
+
+	// GetEvent returns an event by creator and index.
+	GetEvent(creator common.Address, index uint64) *wire.Event
+
+	// LastKnownEvent returns an index of last known creators event.
+	LastKnownEvent(creator common.Address) uint64
 }
--- src/posnode/node_test.go
+++ src/posnode/node_test.go
@@ -1,23 +1,32 @@
 package posnode
 
+//NOTE: mockgen does not work properly out of GOPATH
+//go:generate mockgen -package=posnode -source=consensus.go -destination=mock_test.go Consensus
+
 import (
 	"testing"
+
+	"github.com/golang/mock/gomock"
 
 	"github.com/Fantom-foundation/go-lachesis/src/crypto"
 	"github.com/Fantom-foundation/go-lachesis/src/posnode/network"
 )
 
 func TestNode(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	consensus := NewMockConsensus(ctrl)
+
 	key, err := crypto.GenerateECDSAKey()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// TODO: use New(key, ConsensusMock, network.FakeConnect)
-	n := New(key, nil, nil)
+	n := New(key, consensus, nil) // TODO: network.FakeConnect instead of nil for tests.
 	defer n.Shutdown()
 
-	// TODO: use network.FakeListener("")
+	// TODO: use network.FakeListener("") for tests.
 	listener := network.TcpListener("")
 	n.StartService(listener)
 	defer n.StopService()
