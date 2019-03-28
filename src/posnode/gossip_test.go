23fbfad58721847934d0261519d454f24ad3532c
a.guzev <aguzev@sfxdx.ru>
2019-03-29 09:02:56 +1000

    node gossip test without delay

--- src/posnode/gossip_test.go
+++ src/posnode/gossip_test.go
@@ -3,26 +3,23 @@ package posnode
 import (
 	"strconv"
 	"testing"
-	"time"
 
-	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/Fantom-foundation/go-lachesis/src/crypto"
 	"github.com/Fantom-foundation/go-lachesis/src/posnode/wire"
 )
 
 func TestGossip(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
+	assert := assert.New(t)
 
 	peers := generatePeers(2)
 
 	// Node 1
-	consensus1 := NewMockConsensus(ctrl)
 	store1 := NewMemStore()
 	store1.BootstrapPeers(peers[1:])
 
-	node1 := NewForTests(peers[0].Host, store1, consensus1)
+	node1 := NewForTests(peers[0].Host, store1, nil)
 	defer node1.Shutdown()
 
 	node1.store_SetHeights(&wire.KnownEvents{
@@ -33,15 +30,11 @@ func TestGossip(t *testing.T) {
 	node1.StartServiceForTests()
 	defer node1.StopService()
 
-	node1.StartGossip(2)
-	defer node1.StopGossip()
-
 	// Node 2
-	consensus2 := NewMockConsensus(ctrl)
 	store2 := NewMemStore()
 	store2.BootstrapPeers(peers[:1])
 
-	node2 := NewForTests(peers[1].Host, store2, consensus2)
+	node2 := NewForTests(peers[1].Host, store2, nil)
 	defer node2.Shutdown()
 
 	// Init data for node2
@@ -53,20 +46,15 @@ func TestGossip(t *testing.T) {
 	node2.StartServiceForTests()
 	defer node2.StopService()
 
-	node2.StartGossip(2)
-	defer node2.StopGossip()
+	// gossip
+	node1.syncWithPeer()
+	node2.syncWithPeer()
 
-	<-time.After(3 * time.Second) // TODO: refactor whole test and remove it
-
-	// Check
+	// check
 	heights1 := node1.store_GetHeights()
 	heights2 := node2.store_GetHeights()
 
-	for pID, height := range heights1.Lasts {
-		if heights2.Lasts[pID] != height {
-			t.Fatal("Error: Incorrect gossip data")
-		}
-	}
+	assert.Equal(heights1, heights2, "heights after gossiping")
 }
 
 func generatePeers(count int) []*Peer {
