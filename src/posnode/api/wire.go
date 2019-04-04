2c0797f539d22d692b4e2df23ffb0ff2594a88c4
a.guzev <aguzev@sfxdx.ru>
2019-04-05 02:25:29 +1000

    node api.StartService()

--- src/posnode/api/wire.go
+++ src/posnode/api/wire.go
@@ -12,10 +12,38 @@ package api
 
 import (
 	"context"
+	"math"
 	"net"
 
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/peer"
+
+	"github.com/Fantom-foundation/go-lachesis/src/posnode/network"
 )
+
+// StartService starts and returns gRPC server.
+func StartService(bind string, svc NodeServer, log func(string, ...interface{}), fake bool) (*grpc.Server, string) {
+	server := grpc.NewServer(
+		grpc.MaxRecvMsgSize(math.MaxInt32),
+		grpc.MaxSendMsgSize(math.MaxInt32))
+	RegisterNodeServer(server, svc)
+
+	var listener net.Listener
+	if !fake {
+		listener = network.TcpListener(bind)
+	} else {
+		listener = network.FakeListener(bind)
+	}
+
+	log("service start at %v", listener.Addr())
+	go func() {
+		if err := server.Serve(listener); err != nil {
+			log("service stop (%v)", err)
+		}
+	}()
+
+	return server, listener.Addr().String()
+}
 
 func GrpcPeerHost(ctx context.Context) string {
 	if p, ok := peer.FromContext(ctx); ok {
