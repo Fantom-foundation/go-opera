12a23002ae9e8fc0c28645e636cec744f48ea9cd
a.guzev <aguzev@sfxdx.ru>
2019-04-15 19:44:51 +1000

    network fixes

--- src/network/fake_listener.go
+++ src/network/fake_listener.go
@@ -1,16 +1,20 @@
 package network
 
 import (
+	"fmt"
 	"io"
 	"net"
+	"sync"
 )
 
 // Listener is a fake net listener.
 type Listener struct {
 	NetAddr     Addr
 	connections chan net.Conn
+	sync.RWMutex
 }
 
+// NewListener makes fake listener.
 func NewListener(addr Addr) *Listener {
 	return &Listener{
 		NetAddr:     addr,
@@ -19,10 +23,23 @@ func NewListener(addr Addr) *Listener {
 }
 
 func (l *Listener) connect(from string) (net.Conn, error) {
+	from = from + ":0"
+
+	l.RLock()
+	defer l.RUnlock()
+
+	if l.connections == nil {
+		return nil, &net.OpError{
+			Op:     "connect",
+			Net:    "fake",
+			Source: Addr(from),
+			Addr:   l.Addr(),
+			Err:    fmt.Errorf("closed"),
+		}
+	}
+
 	in1, out1 := io.Pipe()
 	in2, out2 := io.Pipe()
-
-	from = from + ":0"
 
 	conn1 := &Conn{
 		localAddr:  l.NetAddr,
@@ -48,7 +65,20 @@ func (l *Listener) connect(from string) (net.Conn, error) {
 
 // Accept waits for and returns the next connection to the listener.
 func (l *Listener) Accept() (net.Conn, error) {
-	conn, got := <-l.connections
+	l.RLock()
+	connections := l.connections
+	l.RUnlock()
+
+	if connections == nil {
+		return nil, &net.OpError{
+			Op:     "connect",
+			Net:    "fake",
+			Source: l.Addr(),
+			Err:    fmt.Errorf("closed"),
+		}
+	}
+
+	conn, got := <-connections
 	if !got {
 		return nil, &net.AddrError{
 			Err:  "listener closed",
@@ -62,7 +92,21 @@ func (l *Listener) Accept() (net.Conn, error) {
 // Any blocked Accept operations will be unblocked and return errors.
 func (l *Listener) Close() error {
 	removeListener(l)
+
+	l.Lock()
+	defer l.Unlock()
+
+	if l.connections == nil {
+		return &net.OpError{
+			Op:     "close",
+			Net:    "fake",
+			Source: l.Addr(),
+			Err:    fmt.Errorf("closed already"),
+		}
+	}
+
 	close(l.connections)
+	l.connections = nil
 	return nil
 }
 
