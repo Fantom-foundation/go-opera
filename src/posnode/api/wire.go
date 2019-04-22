d3d5e2c4c5db8e4c86368441a352097a190ab23e
devintegral2 <devintegral2@sfxdx.ru>
2019-04-22 15:28:20 +0700

    Add request validation.

--- src/posnode/api/wire.go
+++ src/posnode/api/wire.go
@@ -14,16 +14,24 @@ import (
 	"context"
 	"math"
 	"net"
+	"strconv"
+	"strings"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/peer"
+	"google.golang.org/grpc/status"
 
+	"github.com/Fantom-foundation/go-lachesis/src/common"
+	"github.com/Fantom-foundation/go-lachesis/src/crypto"
 	"github.com/Fantom-foundation/go-lachesis/src/network"
 )
 
 // StartService starts and returns gRPC server.
 func StartService(bind string, svc NodeServer, log func(string, ...interface{}), listen network.ListenFunc) (*grpc.Server, string) {
 	server := grpc.NewServer(
+		grpc.UnaryInterceptor(serverInterceptor),
 		grpc.MaxRecvMsgSize(math.MaxInt32),
 		grpc.MaxSendMsgSize(math.MaxInt32))
 	RegisterNodeServer(server, svc)
@@ -51,4 +59,97 @@ func GrpcPeerHost(ctx context.Context) string {
 		return host
 	}
 	panic("gRPC-peer network address is undefined")
+}
+
+func serverInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	// Get metadata from context
+	_, sign, pub, err := getMetadata(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// Extract common.Pubkey from string
+	key, err := extractPubkey(pub)
+	if err != nil {
+		return nil, err
+	}
+
+	// Check signature of request
+	isInvalid, err := checkSignRequest(req, sign, key)
+	if err != nil {
+		return nil, err
+	}
+
+	if !isInvalid {
+		return nil, status.Errorf(codes.Internal, "Signature is invalid")
+	}
+
+	return handler(ctx, req)
+}
+
+func getMetadata(ctx context.Context) (rID, rSign, rPub string, err error) {
+	// Get metadata from context
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		err = status.Errorf(codes.InvalidArgument, "Retrieving metadata is failed")
+		return
+	}
+
+	// Get ID from metadata
+	// Currently we check only exist status ID and don't use it as is.
+	id, ok := md["client_id"]
+	if !ok {
+		err = status.Errorf(codes.InvalidArgument, "client_id not found")
+		return
+	}
+
+	// Get signature from metadata
+	sign, ok := md["client_sign"]
+	if !ok {
+		err = status.Errorf(codes.InvalidArgument, "client_sign not found")
+		return
+	}
+
+	// Get public key from metadata
+	pub, ok := md["client_pub"]
+	if !ok {
+		err = status.Errorf(codes.InvalidArgument, "client_pub not found")
+		return
+	}
+
+	if id[0] == "" || sign[0] == "" || pub[0] == "" {
+		err = status.Errorf(codes.InvalidArgument, "Empty data")
+		return
+	}
+
+	rID = id[0]
+	rSign = sign[0]
+	rPub = pub[0]
+
+	return
+}
+
+func extractPubkey(pub string) (*common.PublicKey, error) {
+	var bb []byte
+	for _, ps := range strings.Split(strings.Trim(pub, "[]"), " ") {
+		pi, _ := strconv.Atoi(ps)
+		bb = append(bb, byte(pi))
+	}
+
+	key := common.BytesToPubkey(bb)
+	if key == nil {
+		return nil, status.Errorf(codes.Internal, "Pubkey is invalid")
+	}
+
+	return key, nil
+}
+
+func checkSignRequest(req interface{}, sign string, key *common.PublicKey) (bool, error) {
+	hash, _ := req.([]byte)
+	r, s, err := crypto.DecodeSignature(sign)
+	if err != nil {
+		return false, status.Errorf(codes.Internal, "Cannot decode signature")
+	}
+
+	return key.Verify(hash, r, s), nil
 }
--- src/posnode/client.go
+++ src/posnode/client.go
@@ -2,11 +2,22 @@ package posnode
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 
+	"github.com/Fantom-foundation/go-lachesis/src/common"
+	"github.com/Fantom-foundation/go-lachesis/src/crypto"
 	"github.com/Fantom-foundation/go-lachesis/src/posnode/api"
+)
+
+// TODO: find better solution
+// Temp data for client/server Interceptors
+var (
+	clientID = ""
+	key      = new(common.PrivateKey)
 )
 
 type (
@@ -22,10 +33,14 @@ func (n *Node) ConnectTo(peer *Peer) (api.NodeClient, func(), error) {
 	ctx, cancel := context.WithTimeout(context.Background(), n.conf.ConnectTimeout)
 	defer cancel()
 
+	// Fill temp data for Interceptor
+	clientID = n.ID.Hex()
+	key = n.key
+
 	addr := n.NetAddrOf(peer.Host)
 	n.log.Debugf("connect to %s", addr)
 	// TODO: secure connection
-	conn, err := grpc.DialContext(ctx, addr, append(n.client.opts, grpc.WithInsecure(), grpc.WithBlock())...)
+	conn, err := grpc.DialContext(ctx, addr, append(n.client.opts, grpc.WithInsecure(), grpc.WithUnaryInterceptor(clientInterceptor), grpc.WithBlock())...)
 	if err != nil {
 		n.log.Warn(errors.Wrapf(err, "connect to: %s", addr))
 		return nil, nil, err
@@ -36,4 +51,27 @@ func (n *Node) ConnectTo(peer *Peer) (api.NodeClient, func(), error) {
 	}
 
 	return api.NewNodeClient(conn), free, nil
+}
+
+func clientInterceptor(ctx context.Context, method string, req interface{}, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	// Sign request
+	sign, pubKey := signRequest(req)
+
+	// Create new metadata for current request
+	md := metadata.Pairs("client_id", clientID, "client_sign", sign, "client_pub", pubKey)
+
+	// Append new metadata to context
+	ctx = metadata.NewOutgoingContext(ctx, md)
+
+	return invoker(ctx, method, req, reply, cc, opts...)
+}
+
+func signRequest(req interface{}) (sign, pubKey string) {
+	b, _ := req.([]byte)
+	R, S, _ := key.Sign(b)
+
+	sign = crypto.EncodeSignature(R, S)
+	pubKey = fmt.Sprint(key.Public().Bytes())
+
+	return
 }
