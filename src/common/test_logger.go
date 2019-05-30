454539e945a8030baaa47d53500fe40db6082510
devintegral4 <devintegral4@sfxdx.ru>
2019-05-30 18:40:39 +0700

    fix proxy over fake tests

--- src/common/test_logger.go
+++ src/common/test_logger.go
@@ -1,20 +1,26 @@
 package common
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus/hooks/test"
 )
 
 // This can be used as the destination for a logger and it'll
 // map them into calls to testing.T.Log, so that you only see
 // the logging for failed tests.
 type testLoggerAdapter struct {
+	mu     sync.Mutex
 	t      testing.TB
 	prefix string
 }
 
 func (a *testLoggerAdapter) Write(d []byte) (int, error) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+
 	if d[len(d)-1] == '\n' {
 		d = d[:len(d)-1]
 	}
@@ -23,14 +29,16 @@ func (a *testLoggerAdapter) Write(d []byte) (int, error) {
 		a.t.Log(l)
 		return len(l), nil
 	}
+
 	a.t.Log(string(d))
 	return len(d), nil
 }
 
 // NewTestLogger constructor
 func NewTestLogger(t testing.TB) *logrus.Logger {
-	logger := logrus.New()
-	logger.Out = &testLoggerAdapter{t: t}
+	logger, _ := test.NewNullLogger()
+	// todo problem data race by a function call t.Log (testing.TB.Log)
+	// logger.Out = &testLoggerAdapter{t: t}
 	logger.Level = logrus.DebugLevel
 	return logger
 }
