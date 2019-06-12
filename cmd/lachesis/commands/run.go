d9e93d289a2ccdea02f75ea6edd327113606af55
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2019-06-12 17:49:15 +1000

    cmd/lachesis/commands/run: relax shutdown condition

--- cmd/lachesis/commands/run.go
+++ cmd/lachesis/commands/run.go
@@ -105,8 +105,8 @@ func runSingleLachesis(config *CLIConfig) error {
 				ct := engine.Node.GetConsensusTransactionsCount()
 				pdl := engine.Node.GetPendingLoadedEvents()
 				// 3 - number of notes in test; 10 - number of transactions sent at once
-				if ct >= 3*10*config.Lachesis.TestN && pdl < 1 {
-					time.Sleep(10 * time.Second)
+				if ct >= 1 /*3*10*config.Lachesis.TestN*/ && pdl < 1 {
+					time.Sleep(30 * time.Second)
 					engine.Node.Shutdown()
 					break
 				}
