20a39046f67962d0af8c2b49f19dfe947589e48b
bik <dzeckelev@gmail.com>
2019-06-27 15:43:16 +0700

    Add member structure

---
+++ src/posposet/member.go
@@ -0,0 +1,37 @@
+package posposet
+
+import "github.com/Fantom-foundation/go-lachesis/src/crypto"
+
+// Member is a team member.
+type Member struct {
+	publicKey string // string in base64
+	stake     uint64 // In units
+}
+
+// NewMember creates a new member.
+func NewMember(publicKey string, stake uint64) *Member {
+	return &Member{
+		publicKey: publicKey,
+		stake:     stake,
+	}
+}
+
+// SetPublicKey sets a public key.
+func (m *Member) SetPublicKey(key crypto.PublicKey) {
+	m.publicKey = key.Base64()
+}
+
+// SetAmount sets a stake.
+func (m *Member) SetStake(stake uint64) {
+	m.stake = stake
+}
+
+// Amount gets a member stake.
+func (m *Member) Stake() uint64 {
+	return m.stake
+}
+
+// PublicKey gets a member public key.
+func (m *Member) PublicKey() string {
+	return m.publicKey
+}
