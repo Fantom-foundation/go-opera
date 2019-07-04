2bbce26a8cca4b8118d82a5fd5d38dc33a4c7ec7
devintegral3 <devintegral3@sfxdx.ru>
2019-07-04 15:51:37 +0700

    poset: fix store_member

--- src/posposet/store_member.go
+++ src/posposet/store_member.go
@@ -2,20 +2,21 @@ package posposet
 
 import (
 	"github.com/Fantom-foundation/go-lachesis/src/common"
+	"github.com/Fantom-foundation/go-lachesis/src/posposet/members"
 	"github.com/Fantom-foundation/go-lachesis/src/posposet/wire"
 )
 
 // SetMembers stores members of super-frame.
-func (s *Store) SetMembers(n uint64, mm members) {
+func (s *Store) SetMembers(n uint64, mm members.Members) {
 	key := common.IntToBytes(n)
 
 	s.set(s.table.Members, key, mm.ToWire())
 }
 
 // GetMembers returns stored members of super-frame.
-func (s *Store) GetMembers(n uint64) members {
+func (s *Store) GetMembers(n uint64) members.Members {
 	key := common.IntToBytes(n)
 
 	w := s.get(s.table.Members, key, &wire.Members{}).(*wire.Members)
-	return WireToMembers(w)
+	return members.WireToMembers(w)
 }
