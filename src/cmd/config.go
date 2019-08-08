64a234ad8587ff2f1a83321cf509edb16e528b7a
sfxdxdev <gh@sfxdx.ru>
2019-08-09 00:35:54 +1000

    cli with go-ethereum/node

---
+++ src/cmd/config.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"unicode"
+
+	"github.com/ethereum/go-ethereum/cmd/utils"
+	"github.com/ethereum/go-ethereum/node"
+	"github.com/ethereum/go-ethereum/params"
+	"github.com/naoina/toml"
+	cli "gopkg.in/urfave/cli.v1"
+)
+
+var (
+	dumpConfigCommand = cli.Command{
+		Action:      utils.MigrateFlags(dumpConfig),
+		Name:        "dumpconfig",
+		Usage:       "Show configuration values",
+		ArgsUsage:   "",
+		Flags:       nodeFlags,
+		Category:    "MISCELLANEOUS COMMANDS",
+		Description: `The dumpconfig command shows configuration values.`,
+	}
+
+	configFileFlag = cli.StringFlag{
+		Name:  "config",
+		Usage: "TOML configuration file",
+	}
+)
+
+// These settings ensure that TOML keys use the same names as Go struct fields.
+var tomlSettings = toml.Config{
+	NormFieldName: func(rt reflect.Type, key string) string {
+		return key
+	},
+	FieldToKey: func(rt reflect.Type, field string) string {
+		return field
+	},
+	MissingField: func(rt reflect.Type, field string) error {
+		link := ""
+		if unicode.IsUpper(rune(rt.Name()[0])) && rt.PkgPath() != "main" {
+			link = fmt.Sprintf(", see https://godoc.org/%s#%s for available fields", rt.PkgPath(), rt.Name())
+		}
+		return fmt.Errorf("field '%s' is not defined in %s%s", field, rt.String(), link)
+	},
+}
+
+func makeNodeConfig(ctx *cli.Context) *node.Config {
+	cfg := defaultNodeConfig()
+
+	// Apply flags.
+	utils.SetNodeConfig(ctx, &cfg)
+
+	// TODO: move to defaults
+	cfg.P2P.DiscoveryV5 = true
+
+	return &cfg
+}
+
+func defaultNodeConfig() node.Config {
+	cfg := NodeDefaultConfig
+	cfg.Name = clientIdentifier
+	cfg.Version = params.VersionWithCommit(gitCommit, gitDate)
+	cfg.HTTPModules = append(cfg.HTTPModules, "lachesis")
+	cfg.WSModules = append(cfg.WSModules, "lachesis")
+	cfg.IPCPath = "lachesis.ipc"
+	return cfg
+}
+
+// dumpConfig is the dumpconfig command.
+func dumpConfig(ctx *cli.Context) error {
+	cfg := makeNodeConfig(ctx)
+	comment := ""
+
+	out, err := tomlSettings.Marshal(&cfg)
+	if err != nil {
+		return err
+	}
+
+	dump := os.Stdout
+	if ctx.NArg() > 0 {
+		dump, err = os.OpenFile(ctx.Args().Get(0), os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+		if err != nil {
+			return err
+		}
+		defer dump.Close()
+	}
+	dump.WriteString(comment)
+	dump.Write(out)
+
+	return nil
+}
