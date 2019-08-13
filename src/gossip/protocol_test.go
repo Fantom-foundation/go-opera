9d4a122e039be0d502420926386b578a8abfae2c
devintegral3 <devintegral3@sfxdx.ru>
2019-08-13 17:31:31 +0700

    svc: protocol: test: raw tx send/recv

--- src/gossip/protocol_test.go
+++ src/gossip/protocol_test.go
@@ -1,9 +1,13 @@
 package gossip
 
 import (
+	"fmt"
+	"sync"
 	"testing"
 	"time"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/p2p"
 
@@ -71,4 +75,88 @@ func testStatusMsgErrors(t *testing.T, protocol int) {
 		}
 		p.close()
 	}
+}
+
+// This test checks that received transactions are added to the local pool.
+func TestRecvTransactions62(t *testing.T) { testRecvTransactions(t, fantom62) }
+
+func testRecvTransactions(t *testing.T, protocol int) {
+	txAdded := make(chan []*types.Transaction)
+	pm, _ := newTestProtocolManagerMust(t, 5, 5, txAdded)
+	pm.acceptTxs = 1 // mark synced to accept transactions
+	p, _ := newTestPeer("peer", protocol, pm, true)
+	defer pm.Stop()
+	defer p.close()
+
+	tx := newTestTransaction(testAccount, 0, 0)
+	if err := p2p.Send(p.app, TxMsg, []interface{}{tx}); err != nil {
+		t.Fatalf("send error: %v", err)
+	}
+	select {
+	case added := <-txAdded:
+		if len(added) != 1 {
+			t.Errorf("wrong number of added transactions: got %d, want 1", len(added))
+		} else if added[0].Hash() != tx.Hash() {
+			t.Errorf("added wrong tx hash: got %v, want %v", added[0].Hash(), tx.Hash())
+		}
+	case <-time.After(2 * time.Second):
+		t.Errorf("no NewTxsEvent received within 2 seconds")
+	}
+}
+
+// This test checks that pending transactions are sent.
+func TestSendTransactions62(t *testing.T) { testSendTransactions(t, fantom62) }
+
+func testSendTransactions(t *testing.T, protocol int) {
+	pm, _ := newTestProtocolManagerMust(t, 5, 5, nil)
+	defer pm.Stop()
+
+	// Fill the pool with big transactions.
+	const txsize = txsyncPackSize / 10
+	alltxs := make([]*types.Transaction, 100)
+	for nonce := range alltxs {
+		alltxs[nonce] = newTestTransaction(testAccount, uint64(nonce), txsize)
+	}
+	pm.txpool.AddRemotes(alltxs)
+
+	// Connect several peers. They should all receive the pending transactions.
+	var wg sync.WaitGroup
+	checktxs := func(p *testPeer) {
+		defer wg.Done()
+		defer p.close()
+		seen := make(map[common.Hash]bool)
+		for _, tx := range alltxs {
+			seen[tx.Hash()] = false
+		}
+		for n := 0; n < len(alltxs) && !t.Failed(); {
+			var txs []*types.Transaction
+			msg, err := p.app.ReadMsg()
+			if err != nil {
+				t.Errorf("%v: read error: %v", p.Peer, err)
+			} else if msg.Code != TxMsg {
+				t.Errorf("%v: got code %d, want TxMsg", p.Peer, msg.Code)
+			}
+			if err := msg.Decode(&txs); err != nil {
+				t.Errorf("%v: %v", p.Peer, err)
+			}
+			for _, tx := range txs {
+				hash := tx.Hash()
+				seentx, want := seen[hash]
+				if seentx {
+					t.Errorf("%v: got tx more than once: %x", p.Peer, hash)
+				}
+				if !want {
+					t.Errorf("%v: got unexpected tx: %x", p.Peer, hash)
+				}
+				seen[hash] = true
+				n++
+			}
+		}
+	}
+	for i := 0; i < 3; i++ {
+		p, _ := newTestPeer(fmt.Sprintf("peer #%d", i), protocol, pm, true)
+		wg.Add(1)
+		go checktxs(p)
+	}
+	wg.Wait()
 }
