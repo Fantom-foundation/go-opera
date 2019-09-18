809077fca89557b8a020427f1a6e22b54a7d3a93
devintegral2 <devintegral2@sfxdx.ru>
2019-09-18 12:34:51 +0700

    upper limit for values to prevent overflow

--- src/poset/frame_info.go
+++ src/poset/frame_info.go
@@ -2,6 +2,9 @@ package poset
 
 import (
 	"github.com/Fantom-foundation/go-lachesis/src/inter"
+	"github.com/ethereum/go-ethereum/rlp"
+	"io"
+	"math"
 )
 
 // TODO: make FrameInfo internal
@@ -9,6 +12,28 @@ import (
 type FrameInfo struct {
 	TimeOffset int64 // may be negative
 	TimeRatio  inter.Timestamp
+}
+
+type frameInfoMarshaling struct {
+	TimeOffset uint64
+	TimeRatio  inter.Timestamp
+}
+
+func (f *FrameInfo) EncodeRLP(w io.Writer) error {
+	return rlp.Encode(w, frameInfoMarshaling{
+		TimeOffset: uint64(f.TimeOffset + math.MaxInt64/2),
+		TimeRatio:  f.TimeRatio,
+	})
+}
+
+func (f *FrameInfo) DecodeRLP(st *rlp.Stream) error {
+	m := frameInfoMarshaling{}
+	if err := st.Decode(&m); err != nil {
+		return err
+	}
+	f.TimeOffset = int64(m.TimeOffset) - math.MaxInt64/2
+	f.TimeRatio = m.TimeRatio
+	return nil
 }
 
 // GetConsensusTimestamp calc consensus timestamp for given event.
--- src/poset/frame_info_test.go
+++ src/poset/frame_info_test.go
@@ -3,6 +3,7 @@ package poset
 import (
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/stretchr/testify/assert"
+	"math"
 	"testing"
 )
 
@@ -10,8 +11,25 @@ func TestFrameInfoSerialization(t *testing.T) {
 	assertar := assert.New(t)
 
 	f0 := &FrameInfo{
-		TimeOffset: 3,
-		TimeRatio:  1,
+		TimeOffset: math.MaxInt64 / 2,
+		TimeRatio:  math.MaxUint64,
+	}
+	buf, err := rlp.EncodeToBytes(f0)
+	assertar.NoError(err)
+
+	f1 := &FrameInfo{}
+	err = rlp.DecodeBytes(buf, f1)
+	assertar.NoError(err)
+
+	assertar.EqualValues(f0, f1)
+}
+
+func TestFrameInfoSerializationSigned(t *testing.T) {
+	assertar := assert.New(t)
+
+	f0 := &FrameInfo{
+		TimeOffset: -math.MaxInt64 / 2,
+		TimeRatio:  0,
 	}
 	buf, err := rlp.EncodeToBytes(f0)
 	assertar.NoError(err)
