05c7d3666c2cac661a9852f4790dda86d97a5b44
sfxdxdev <gh@sfxdx.ru>
2019-09-30 17:37:51 +1000

    docker swarm client

---
+++ src/docker/swarm/set_env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+REGISTRY_HOST=18.189.195.64
+SWARM_HOST=18.189.195.64
+SSLDIR=$(dirname $0)/ssl
+
+CA_FILE=`ls -1 ${SSLDIR}/*CA*.crt`
+CERT_FILE=`ls -1 ${SSLDIR}/*.crt | grep -v CA`
+KEY_FILE=`ls -1 ${SSLDIR}/*.key`
+
+SWARM="--tls --tlscacert=${CA_FILE} --tlscert=${CERT_FILE} --tlskey=${KEY_FILE} -H tcp://${SWARM_HOST}:2376"
