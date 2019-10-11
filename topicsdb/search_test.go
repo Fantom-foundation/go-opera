c35801ea7611e2a12c8c3002db0bea46aa8327c9
devintegral3 <devintegral3@sfxdx.ru>
2019-10-12 02:05:03 +1000

    BenchmarkSearch()

---
+++ topicsdb/search_test.go
@@ -0,0 +1,62 @@
+package topicsdb
+
+import (
+	"testing"
+	"time"
+
+	"github.com/Fantom-foundation/go-lachesis/kvdb/memorydb"
+)
+
+func BenchmarkSearch(b *testing.B) {
+	topics, recs, topics4rec := genTestData()
+
+	mem := memorydb.New()
+	mem.SetDelay(1 * time.Millisecond)
+	db := New(mem)
+
+	for _, rec := range recs {
+		if err := db.Push(rec); err != nil {
+			b.Fatal(err)
+		}
+	}
+
+	var conditionsSet [][]Condition
+	for i := 0; i < len(topics); i++ {
+		from, to := topics4rec(i)
+		tt := topics[from : to-1]
+
+		conditions := make([]Condition, len(tt))
+		for n, t := range tt {
+			conditions[n] = NewCondition(t.Val, n)
+		}
+
+		conditionsSet = append(conditionsSet, conditions)
+	}
+
+	b.Run("Sync", func(b *testing.B) {
+		db.fetchMethod = db.fetchSync
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			conditions := conditionsSet[i%len(conditionsSet)]
+			_, err := db.Find(conditions...)
+			if err != nil {
+				b.Fatal(err)
+			}
+		}
+	})
+
+	b.Run("Async", func(b *testing.B) {
+		db.fetchMethod = db.fetchAsync
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			conditions := conditionsSet[i%len(conditionsSet)]
+			_, err := db.Find(conditions...)
+			if err != nil {
+				b.Fatal(err)
+			}
+		}
+	})
+
+}
