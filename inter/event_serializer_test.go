261dbd39b0aff5d7900328c8ccbbd6610c2beeca
devintegral2 <devintegral2@sfxdx.ru>
2019-10-16 11:48:58 +0300

    Final release for EventHeaderData custom serializer

--- inter/event_serializer_test.go
+++ inter/event_serializer_test.go
@@ -12,138 +12,30 @@ import (
 	"github.com/Fantom-foundation/go-lachesis/inter/idx"
 )
 
-func TestEventHeaderData_Serialize(t *testing.T) {
+func TestEventHeaderData_MarshalBinary(t *testing.T) {
 	events := FakeEventWithOneEpoch()
 	header := events[len(events)-1].EventHeaderData
 
-	buf := header.Serialize()
+	buf, _ := header.MarshalBinary()
 	newHeader := EventHeaderData{}
-	newHeader.Deserialize(buf)
+	_ = newHeader.UnmarshalBinary(buf)
 
 	assert.EqualValues(t, header, newHeader)
 }
 
-func TestEventHeaderData_SerializeA(t *testing.T) {
+func BenchmarkEventHeaderData_MarshalBinary(b *testing.B) {
 	events := FakeEventWithOneEpoch()
 	header := events[len(events)-1].EventHeaderData
 
-	buf := header.SerializeA()
-	newHeader := EventHeaderData{}
-	newHeader.DeserializeA(buf)
-
-	assert.EqualValues(t, header, newHeader)
-}
-
-func TestEventHeaderData_SerializeB(t *testing.T) {
-	events := FakeEventWithOneEpoch()
-	header := events[len(events)-1].EventHeaderData
-
-	buf := header.SerializeB()
-	newHeader := EventHeaderData{}
-	newHeader.DeserializeB(buf)
-
-	assert.EqualValues(t, header, newHeader)
-}
-
-func TestEventHeaderData_SerializeC(t *testing.T) {
-	events := FakeEventWithOneEpoch()
-	header := events[len(events)-1].EventHeaderData
-
-	buf := header.SerializeC()
-	newHeader := EventHeaderData{}
-	newHeader.DeserializeC(buf)
-
-	assert.EqualValues(t, header, newHeader)
-}
-
-func BenchmarkEventHeaderData_Serialize(b *testing.B) {
-	events := FakeEventWithOneEpoch()
-	header := events[len(events)-1].EventHeaderData
-
-	buf := header.Serialize()
-	b.ReportMetric(float64(len(*buf)), "Bytes")
-
-	/*
-	if b.N == 1 {
-		fmt.Printf("Serialized: %X\n", buf)
-
-		tmp, err := rlp.EncodeToBytes(header)
-		if err != nil {
-			b.Fatalf("Error rlp serialization: %s", err)
-		}
-
-		fmt.Printf("OldSerialized: %X\n", tmp)
-	}
-	*/
+	buf, _ := header.MarshalBinary()
+	b.ReportMetric(float64(len(buf)), "Bytes")
 
 	for i := 0; i < b.N; i++ {
-		_ = header.Serialize()
+		_, _ = header.MarshalBinary()
 	}
 }
 
-func BenchmarkEventHeaderData_SerializeA(b *testing.B) {
-	events := FakeEventWithOneEpoch()
-	header := events[len(events)-1].EventHeaderData
-
-	buf := header.SerializeA()
-	b.ReportMetric(float64(len(*buf)), "Bytes")
-
-	/*
-	if b.N == 1 {
-		fmt.Printf("Serialized: %X\n", buf)
-
-		tmp, err := rlp.EncodeToBytes(header)
-		if err != nil {
-			b.Fatalf("Error rlp serialization: %s", err)
-		}
-
-		fmt.Printf("OldSerialized: %X\n", tmp)
-	}
-	*/
-
-	for i := 0; i < b.N; i++ {
-		_ = header.SerializeA()
-	}
-}
-
-func BenchmarkEventHeaderData_SerializeB(b *testing.B) {
-	events := FakeEventWithOneEpoch()
-	header := events[len(events)-1].EventHeaderData
-
-	buf := header.SerializeB()
-	b.ReportMetric(float64(len(*buf)), "Bytes")
-
-	/*
-		if b.N == 1 {
-			fmt.Printf("Serialized: %X\n", buf)
-
-			tmp, err := rlp.EncodeToBytes(header)
-			if err != nil {
-				b.Fatalf("Error rlp serialization: %s", err)
-			}
-
-			fmt.Printf("OldSerialized: %X\n", tmp)
-		}
-	*/
-
-	for i := 0; i < b.N; i++ {
-		_ = header.SerializeB()
-	}
-}
-
-func BenchmarkEventHeaderData_SerializeC(b *testing.B) {
-	events := FakeEventWithOneEpoch()
-	header := events[len(events)-1].EventHeaderData
-
-	buf := header.SerializeC()
-	b.ReportMetric(float64(len(*buf)), "Bytes")
-
-	for i := 0; i < b.N; i++ {
-		_ = header.SerializeC()
-	}
-}
-
-func BenchmarkEventHeaderData_OldSerialize(b *testing.B) {
+func BenchmarkEventHeaderData_OldMarshal(b *testing.B) {
 	events := FakeEventWithOneEpoch()
 	header := events[len(events)-1].EventHeaderData
 
@@ -158,50 +50,18 @@ func BenchmarkEventHeaderData_OldSerialize(b *testing.B) {
 	}
 }
 
-func BenchmarkEventHeaderData_Deserialize(b *testing.B) {
+func BenchmarkEventHeaderData_UnmarshalBinary(b *testing.B) {
 	events := FakeEventWithOneEpoch()
 	header := events[len(events)-1].EventHeaderData
 
-	buf := header.Serialize()
+	buf, _ := header.MarshalBinary()
 
 	for i := 0; i < b.N; i++ {
-		header.Deserialize(buf)
+		_ = header.UnmarshalBinary(buf)
 	}
 }
 
-func BenchmarkEventHeaderData_DeserializeA(b *testing.B) {
-	events := FakeEventWithOneEpoch()
-	header := events[len(events)-1].EventHeaderData
-
-	buf := header.SerializeA()
-
-	for i := 0; i < b.N; i++ {
-		header.DeserializeA(buf)
-	}
-}
-
-func BenchmarkEventHeaderData_DeserializeB(b *testing.B) {
-	events := FakeEventWithOneEpoch()
-	header := events[len(events)-1].EventHeaderData
-
-	buf := header.SerializeB()
-
-	for i := 0; i < b.N; i++ {
-		header.DeserializeB(buf)
-	}
-}
-
-func BenchmarkEventHeaderData_DeserializeC(b *testing.B) {
-	events := FakeEventWithOneEpoch()
-	header := events[len(events)-1].EventHeaderData
-
-	buf := header.SerializeC()
-
-	for i := 0; i < b.N; i++ {
-		header.DeserializeC(buf)
-	}
-}
-func BenchmarkEventHeaderData_OldDeserialize(b *testing.B) {
+func BenchmarkEventHeaderData_OldUnmarshal(b *testing.B) {
 	events := FakeEventWithOneEpoch()
 	header := events[len(events)-1].EventHeaderData
 
@@ -253,7 +113,7 @@ func FakeEventEpoch() (h hash.Event) {
 	if err != nil {
 		panic(err)
 	}
-	copy(h[0:4], []byte{1,0,1,0})
+	copy(h[0:4], []byte{1, 0, 1, 0})
 	return
 }
 
