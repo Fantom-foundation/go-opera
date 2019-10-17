7e3e5006d78f3e4201cce7a3b45a119cf71c821e
sfxdxdev <gh@sfxdx.ru>
2019-10-17 14:24:26 +1000

    codacy fixes

--- docker/start.sh
+++ docker/start.sh
@@ -21,35 +21,38 @@ do
 done
 
 attach_and_exec() {
-    local name=$1
-    local cmd=$2
+    local NAME=$1
+    local CMD=$2
 
-    for attempt in `seq 10`
+    for attempt in $(seq 10)
     do
-        res=$(docker exec -i ${name} /lachesis --exec "$cmd" attach http://127.0.0.1:18545)
+        echo -n "  - attempt ${attempt}: " >&2
+        res=$(docker exec -i ${NAME} /lachesis --exec "${CMD}" attach http://127.0.0.1:18545)
         if [ $? -eq 0 ]
         then
+            echo " ok" >&2
             echo $res
             break
         else
-            echo "    try ${attempt} to attach console" >&2
             sleep 1
         fi
     done
 }
 
-echo -e "\nConnect nodes (ring):\n"
+echo -e "\nConnect nodes to ring:\n"
 for i in $(seq $N)
 do
     j=$((i % N + 1))
-    echo " connect $i to $j"
 
+    echo " getting node-$j address:"
     ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${NAME}-$j)
-
     enode=$(attach_and_exec ${NAME}-$j 'admin.nodeInfo.enode')
-    enode=$(echo $enode | sed "s/127.0.0.1/${ip}/")
+    enode=$(echo ${enode} | sed "s/127.0.0.1/${ip}/")
+    echo "    address = ${enode}"
 
-    attach_and_exec ${NAME}-$i "admin.addPeer(${enode})"
+    echo " connecting node-$i to node-$j:"
+    res=$(attach_and_exec ${NAME}-$i "admin.addPeer(${enode})")
+    echo "    result = ${res}"
 done
 
 
