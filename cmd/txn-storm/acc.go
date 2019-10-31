b31a08287265f1144da3557ca25dcc9441f5b807
sfxdxdev <gh@sfxdx.ru>
2019-11-01 00:55:15 +1000

    account

---
+++ cmd/txn-storm/acc.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/params"
+
+	"github.com/Fantom-foundation/go-lachesis/crypto"
+)
+
+var (
+	gasLimit = uint64(21000)
+	gasPrice = big.NewInt(0)
+)
+
+type Acc struct {
+	Key  *ecdsa.PrivateKey
+	Addr *common.Address
+}
+
+func MakeAcc(n uint) *Acc {
+	key := crypto.FakeKey(int(n))
+	addr := crypto.PubkeyToAddress(key.PublicKey)
+
+	return &Acc{
+		Key:  key,
+		Addr: &addr,
+	}
+}
+
+func (a *Acc) TransactionTo(b *Acc, nonce, amount uint) *types.Transaction {
+	txn := types.NewTransaction(
+		uint64(nonce),
+		*b.Addr,
+		big.NewInt(int64(amount)),
+		gasLimit,
+		gasPrice,
+		[]byte{},
+	)
+
+	signed, err := types.SignTx(
+		txn,
+		types.NewEIP155Signer(params.AllEthashProtocolChanges.ChainID),
+		a.Key,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	return signed
+}
