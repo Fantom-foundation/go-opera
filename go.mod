96fbc5ef3f44792363569c79a642ec83fee020be
sfxdxdev <gh@sfxdx.ru>
2019-11-07 02:18:05 +1000

    prometheus metrics adapter

--- go.mod
+++ go.mod
@@ -48,6 +48,7 @@ require (
 	github.com/peterh/liner v1.1.0 // indirect
 	github.com/pkg/errors v0.8.1
 	github.com/prometheus/client_golang v1.0.0
+	github.com/prometheus/client_model v0.0.0-20190129233127-fd36f4220a90
 	github.com/prometheus/tsdb v0.10.0 // indirect
 	github.com/rjeczalik/notify v0.9.2 // indirect
 	github.com/robertkrimen/otto v0.0.0-20180617131154-15f95af6e78d // indirect
--- metrics/prometheus/metrics.go
+++ metrics/prometheus/metrics.go
@@ -33,66 +33,29 @@ func convertToPrometheusMetric(name string, m interface{}) (prometheus.Collector
 			return metric.Value()
 		})
 
+	case metrics.Healthcheck:
+		collector = prometheus.NewGaugeFunc(prometheus.GaugeOpts(opts), func() float64 {
+			metric.Check()
+			if err := metric.Error(); nil != err {
+				return 1
+			}
+			return 0
+		})
+
 	// TODO: do
 	case metrics.Meter:
-		return nil, false
-		/*m := metric.Snapshot()
-		values["count"] = m.Count()
-		values["1m.rate"] = m.Rate1()
-		values["5m.rate"] = m.Rate5()
-		values["15m.rate"] = m.Rate15()
-		values["mean.rate"] = m.RateMean()*/
-
-		// TODO: do
-	case metrics.Healthcheck:
-		return nil, false
-	/*
-		values["error"] = nil
-		metric.Check()
-		if err := metric.Error(); nil != err {
-			values["error"] = metric.Error().Error()
-		}
-	*/
+		collector = NewCollector(opts, metric,
+			"rate1m", "rate5m", "rate15m", "rate")
 
 	// TODO: do
 	case metrics.Histogram:
-		return nil, false
-	/*
-		h := metric.Snapshot()
-		ps := h.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
-		values["count"] = h.Count()
-		values["min"] = h.Min()
-		values["max"] = h.Max()
-		values["mean"] = h.Mean()
-		values["stddev"] = h.StdDev()
-		values["median"] = ps[0]
-		values["75%"] = ps[1]
-		values["95%"] = ps[2]
-		values["99%"] = ps[3]
-		values["99.9%"] = ps[4]
-	*/
+		collector = NewCollector(opts, metric,
+			"min", "max", "mean")
 
 	// TODO: do
-	case metrics.Timer:
-		return nil, false
-		/*
-			t := metric.Snapshot()
-			ps := t.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
-			values["count"] = t.Count()
-			values["min"] = t.Min()
-			values["max"] = t.Max()
-			values["mean"] = t.Mean()
-			values["stddev"] = t.StdDev()
-			values["median"] = ps[0]
-			values["75%"] = ps[1]
-			values["95%"] = ps[2]
-			values["99%"] = ps[3]
-			values["99.9%"] = ps[4]
-			values["1m.rate"] = t.Rate1()
-			values["5m.rate"] = t.Rate5()
-			values["15m.rate"] = t.Rate15()
-			values["mean.rate"] = t.RateMean()
-		*/
+	case metrics.Timer, metrics.ResettingTimer:
+		collector = NewCollector(opts, metric,
+			"min", "max", "mean", "rate1m", "rate5m", "rate15m", "rate")
 
 	default:
 		logger.Warn("metric doesn't support prometheus",
