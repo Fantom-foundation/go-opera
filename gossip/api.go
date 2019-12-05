1b7cae8e535ab46d3389542d47b3a28a4f55a898
devintegral2 <devintegral2@sfxdx.ru>
2019-12-05 12:53:01 +0300

    Fix comment

--- gossip/api.go
+++ gossip/api.go
@@ -32,7 +32,7 @@ func (api *PublicEthereumAPI) Hashrate() hexutil.Uint64 {
 	return hexutil.Uint64(0)
 }
 
-// ChainID is the EIP-155 replay-protection chain id for the current ethereum chain config.
+// ChainId is the EIP-155 replay-protection chain id for the current ethereum chain config.
 func (api *PublicEthereumAPI) ChainId() hexutil.Uint64 {
 	return hexutil.Uint64(params.AllEthashProtocolChanges.ChainID.Uint64())
 }
