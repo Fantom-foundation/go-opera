1756ee41b7c02d48aff683e137412ba255c01634
devintegral3 <devintegral3@sfxdx.ru>
2020-01-10 17:22:48 +0700

    add v5 bootnodes

--- cmd/lachesis/params.go
+++ cmd/lachesis/params.go
@@ -5,30 +5,23 @@ import (
 )
 
 func overrideParams() {
-	// MainnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
-	// the main Ethereum network.
-	params.MainnetBootnodes = []string{}
-
-	// TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the
-	// Ropsten test network.
-	params.TestnetBootnodes = []string{}
-
-	// RinkebyBootnodes are the enode URLs of the P2P bootstrap nodes running on the
-	// Rinkeby test network.
-	params.RinkebyBootnodes = []string{}
-
-	// GoerliBootnodes are the enode URLs of the P2P bootstrap nodes running on the
-	// Görli test network.
-	params.GoerliBootnodes = []string{}
-
 	// DiscoveryV5Bootnodes are the enode URLs of the P2P bootstrap nodes for the
 	// experimental RLPx v5 topic-discovery network.
 	params.DiscoveryV5Bootnodes = []string{
-		/*
-			"enode://06051a5573c81934c9554ef2898eb13b33a34b94cf36b202b69fde139ca17a85051979867720d4bdae4323d4943ddf9aeeb6643633aa656e0be843659795007a@35.177.226.168:30303",
-			"enode://0cc5f5ffb5d9098c8b8c62325f3797f56509bff942704687b6530992ac706e2cb946b90a34f1f19548cd3c7baccbcaea354531e5983c7d1bc0dee16ce4b6440b@40.118.3.223:30304",
-			"enode://1c7a64d76c0334b0418c004af2f67c50e36a3be60b5e4790bdac0439d21603469a85fad36f2473c9a80eb043ae60936df905fa28f1ff614c3e5dc34f15dcd2dc@40.118.3.223:30306",
-			"enode://85c85d7143ae8bb96924f2b54f1b3e70d8c4d367af305325d30a61385a432f247d2c75c45c6b4a60335060d072d7f5b35dd1d4c45f76941f62a4f83b6e75daaf@40.118.3.223:30307",
-		*/
+		"enode://a2941866e485442aa6b17d67d77f8a6c4580bb556894cc1618473eff1e18203d8cce50b563cf4c75e408886079b8f067069442ed52e2ac9e556baa3f8fcc525f@3.24.15.66:5050",
+		"enode://9ea4e8ad12e1fcfc846d00a626fbf3ca1ee6a5143148b8472dd62a64b876d01031c064a717aaa606de34bcbbb691b50cbdf816acd9e9ee7ce334082b739829b9@52.45.126.213:5050",
+		"enode://cba8bd4179052908d069c6bacccf999dd576fdfe6fd29db19b21ae262d96a9ac00a3f5904b772e44b172575b8afbf91c0d1303f1b7b03dfae14e50e234004d36@15.164.136.219:5050",
+		"enode://fb904114975d7b238c2d5e46824ac00fdd1133f836e6e5332765e089e70c6dbd4d47c513fcb2ee72774484216634cc9785a54816bfde46d721871d340070b64b@34.245.17.87:5050",
 	}
+
+	// MainnetBootnodes are the enode URLs of the discovery V4 P2P bootstrap nodes running on
+	// the main Opera network.
+	params.MainnetBootnodes = []string{}
+
+	// TestnetBootnodes are the enode URLs of the discovery V4 P2P bootstrap nodes running on the
+	// Testnet test network.
+	params.TestnetBootnodes = []string{}
+
+	params.RinkebyBootnodes = []string{}
+	params.GoerliBootnodes = []string{}
 }
