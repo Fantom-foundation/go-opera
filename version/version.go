43db72fb1dfc61bd90b1ef48658114769d5c78a8
sfxdxdev <gh@sfxdx.ru>
2020-01-24 17:13:05 +0700

    fix version = 0.5.0.rc2

--- version/version.go
+++ version/version.go
@@ -5,8 +5,8 @@ import (
 )
 
 func init() {
-	params.VersionMajor = 0       // Major version component of the current release
-	params.VersionMinor = 5       // Minor version component of the current release
-	params.VersionPatch = 0       // Patch version component of the current release
-	params.VersionMeta = "stable" // Version metadata to append to the version string
+	params.VersionMajor = 0    // Major version component of the current release
+	params.VersionMinor = 5    // Minor version component of the current release
+	params.VersionPatch = 0    // Patch version component of the current release
+	params.VersionMeta = "rc2" // Version metadata to append to the version string
 }
