23cad0762195445f530edf488418a4d4485fc33d
Egor Lysenko <uprendis@gmail.com>
2020-09-09 09:57:12 +0700

    update parentscheck to use lachesis.parentscheck

--- eventcheck/parentscheck/parents_check.go
+++ eventcheck/parentscheck/parents_check.go
@@ -3,74 +3,41 @@ package parentscheck
 import (
 	"errors"
 
-	"github.com/Fantom-foundation/go-lachesis/inter"
-	"github.com/Fantom-foundation/go-lachesis/inter/idx"
-	"github.com/Fantom-foundation/go-lachesis/lachesis"
+	base "github.com/Fantom-foundation/lachesis-base/eventcheck/parentscheck"
+
+	"github.com/Fantom-foundation/go-opera/inter"
 )
 
 var (
-	ErrWrongSeq        = errors.New("event has wrong sequence time")
-	ErrWrongLamport    = errors.New("event has wrong Lamport time")
-	ErrDoubleParents   = errors.New("event has double parents")
-	ErrWrongSelfParent = errors.New("event is missing self-parent")
-	ErrPastTime        = errors.New("event has lower claimed time than self-parent")
+	ErrPastTime = errors.New("event has lower claimed time than self-parent")
 )
 
 // Checker which require only parents list + current epoch info
 type Checker struct {
-	config *lachesis.DagConfig
+	base *base.Checker
 }
 
 // New validator which performs checks, which require known the parents
-func New(config *lachesis.DagConfig) *Checker {
+func New() *Checker {
 	return &Checker{
-		config: config,
+		base: &base.Checker{},
 	}
 }
 
 // Validate event
-func (v *Checker) Validate(e *inter.Event, parents []*inter.EventHeaderData) error {
-	if len(e.Parents) != len(parents) {
-		panic("parentscheck: expected event's parents as an argument")
+func (v *Checker) Validate(e inter.EventI, parents inter.EventIs) error {
+	if err := v.base.Validate(e, parents.Bases()); err != nil {
+		return err
 	}
 
-	// lamport
-	maxLamport := idx.Lamport(0)
-	for _, p := range parents {
-		maxLamport = idx.MaxLamport(maxLamport, p.Lamport)
-	}
-	if e.Lamport != maxLamport+1 {
-		return ErrWrongLamport
-	}
-
-	// parents
-	if len(e.Parents.Set()) != len(e.Parents) {
-		return ErrDoubleParents
-	}
-
-	// self-parent
-	for i, p := range parents {
-		if (p.Creator == e.Creator) != e.IsSelfParent(e.Parents[i]) {
-			return ErrWrongSelfParent
-		}
-	}
-
-	// seq
-	if (e.Seq <= 1) != (e.SelfParent() == nil) {
-		return ErrWrongSeq
-	}
 	if e.SelfParent() != nil {
 		selfParent := parents[0]
-		if !e.IsSelfParent(selfParent.Hash()) {
+		if !e.IsSelfParent(selfParent.ID()) {
 			// sanity check, self-parent is always first, it's how it's stored
-			return ErrWrongSelfParent
+			return base.ErrWrongSelfParent
 		}
-		if e.Seq != selfParent.Seq+1 {
-			return ErrWrongSeq
-		}
-
 		// selfParent time
-		if e.ClaimedTime <= selfParent.ClaimedTime {
+		if e.CreationTime() <= selfParent.CreationTime() {
 			return ErrPastTime
 		}
 	}
