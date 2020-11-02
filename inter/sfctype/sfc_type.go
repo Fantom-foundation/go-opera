edcbdf92f3997018ca21dc535831ea7eb2998814
Egor Lysenko <uprendis@gmail.com>
2020-11-02 14:21:05 +0300

    types for SFC 3.x

--- inter/sfctype/sfc_type.go
+++ inter/sfctype/sfc_type.go
@@ -4,65 +4,24 @@ import (
 	"math/big"
 
 	"github.com/Fantom-foundation/lachesis-base/inter/idx"
-	"github.com/ethereum/go-ethereum/common"
 
-	"github.com/Fantom-foundation/go-opera/inter"
 	"github.com/Fantom-foundation/go-opera/inter/validator"
 )
 
 var (
-	// ForkBit is set if staker has a confirmed pair of fork events
-	ForkBit = uint64(1)
-	// OfflineBit is set if staker has didn't have confirmed events for a long time
-	OfflineBit = uint64(1 << 8)
-	// CheaterMask is a combination of severe misbehavings
-	CheaterMask = ForkBit
+	// DoublesignBit is set if validator has a confirmed pair of fork events
+	DoublesignBit = uint64(1 << 7)
+	OkStatus      = uint64(0)
 )
 
-// SfcStaker is the node-side representation of SFC staker
-type SfcStaker struct {
-	CreatedEpoch idx.Epoch
-	CreationTime inter.Timestamp
-
-	DeactivatedEpoch idx.Epoch
-	DeactivatedTime  inter.Timestamp
-
-	StakeAmount *big.Int
-	DelegatedMe *big.Int
-
-	Address common.Address
-	PubKey  validator.PubKey
-
-	Status uint64
+// SfcValidator is the node-side representation of SFC validator
+type SfcValidator struct {
+	Weight *big.Int
+	Pubkey validator.PubKey
 }
 
-// Ok returns true if not deactivated and not pruned
-func (s *SfcStaker) Ok() bool {
-	return s.Status == 0 && s.DeactivatedEpoch == 0
-}
-
-// IsCheater returns true if staker is cheater
-func (s *SfcStaker) IsCheater() bool {
-	return s.Status&CheaterMask != 0
-}
-
-// HasFork returns true if staker has a confirmed fork
-func (s *SfcStaker) HasFork() bool {
-	return s.Status&ForkBit != 0
-}
-
-// Offline returns true if staker was offline for long time
-func (s *SfcStaker) Offline() bool {
-	return s.Status&OfflineBit != 0
-}
-
-// SfcStakerAndID is pair SfcStaker + ValidatorID
-type SfcStakerAndID struct {
+// SfcValidatorAndID is pair SfcValidator + ValidatorID
+type SfcValidatorAndID struct {
 	ValidatorID idx.ValidatorID
-	Staker      *SfcStaker
-}
-
-// CalcTotalStake returns sum of staker's stake and delegated to staker stake
-func (s *SfcStaker) CalcTotalStake() *big.Int {
-	return new(big.Int).Add(s.StakeAmount, s.DelegatedMe)
+	Validator   SfcValidator
 }
