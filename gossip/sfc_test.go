fa1513c9ebd902c548f09a0d303ae38a1ff7a793
alex <rusdev.alex@gmail.com>
2020-11-24 19:02:44 +1000

    truffle compile to get dependencies then solc (WIP)

--- gossip/sfc_test.go
+++ gossip/sfc_test.go
@@ -4,13 +4,13 @@ package gossip
 // NOTE: assumed that opera-sfc repo is in the same dir than go-opera repo
 
 // sfc proxy
-//go:generate bash -c "cd ../../opera-sfc && git checkout main && docker run --rm -v $(pwd):/src -v $(pwd)/../go-opera/gossip/contract:/dst ethereum/solc:0.5.12 -o /dst/solc/ --optimize --optimize-runs=2000 --bin --abi --allow-paths /src/contracts --overwrite /src/contracts/sfc/Migrations.sol"
-//go:generate mkdir -p ./contract/sfcproxy
-//go:generate go run github.com/ethereum/go-ethereum/cmd/abigen --bin=./contract/solc/Migrations.bin --abi=./contract/solc/Migrations.abi --pkg=sfcproxy --type=Contract --out=contract/sfcproxy/contract.go
+//1go:generate bash -c "cd ../../opera-sfc && git checkout main && docker run --rm -v $(pwd):/src -v $(pwd)/../go-opera/gossip/contract:/dst ethereum/solc:0.5.12 -o /dst/solc/ --optimize --optimize-runs=2000 --bin --abi --allow-paths /src/contracts --overwrite /src/contracts/sfc/Migrations.sol"
+//1go:generate mkdir -p ./contract/sfcproxy
+//1go:generate go run github.com/ethereum/go-ethereum/cmd/abigen --bin=./contract/solc/Migrations.bin --abi=./contract/solc/Migrations.abi --pkg=sfcproxy --type=Contract --out=contract/sfcproxy/contract.go
 
 // main (genesis)
-//go:generate bash -c "cd ../../opera-sfc && git checkout main && docker run --rm -v $(pwd):/src -w /src node:10.23.0 bash -c 'npm install -g truffle@v5.1.4 && truffle build'"
-//go:generate bash -c "cd ../../opera-sfc && docker run --rm -v $(pwd):/src -v $(pwd)/../go-opera/gossip/contract:/dst ethereum/solc:0.5.12 -o /dst/solc/ --optimize --optimize-runs=2000 --bin --abi --allow-paths /src --overwrite /src/contracts/sfc/SFC.sol"
+//go:generate bash -c "NPM_CONFIG_PREFIX=~ cd ../../opera-sfc && git checkout main && docker run --rm -v $(pwd)/../go-opera/gossip/contract/solc:/src/build/contracts -v $(pwd):/src -w /src node:10.23.0 bash -c 'npm install -g truffle@v5.1.4 && npm install && truffle compile --all'"
+//go:generate bash -c "cd ../../opera-sfc && docker run --rm -v $(pwd):/src -v $(pwd)/../go-opera/gossip/contract:/dst ethereum/solc:0.5.12 @openzeppelin/contracts/math=/src/node_modules/@openzeppelin/contracts/math --optimize --optimize-runs=2000 --bin --abi --allow-paths /src --overwrite -o /dst/solc/ /src/contracts/sfc/SFC.sol"
 //go:generate mkdir -p ./contract/sfc100
 //go:generate go run github.com/ethereum/go-ethereum/cmd/abigen --bin=./contract/solc/SFC.bin --abi=./contract/solc/SFC.abi --pkg=sfc100 --type=Contract --out=contract/sfc100/contract.go
 
