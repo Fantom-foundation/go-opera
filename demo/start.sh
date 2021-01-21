ee9de0686c79994fbdb38ef71c18a26cb5c9acab
Egor Lysenko <uprendis@gmail.com>
2021-01-21 17:59:22 +0700

    fix demo API namespace

--- demo/start.sh
+++ demo/start.sh
@@ -24,8 +24,8 @@ do
 	--fakenet=${ACC}/$N \
 	--port=${PORT} \
 	--nat extip:127.0.0.1 \
-	--http --http.addr="127.0.0.1" --http.port=${RPCP} --http.corsdomain="*" --http.api="eth,debug,net,admin,web3,personal,txpool,ftm,sfc" \
-	--ws --ws.addr="127.0.0.1" --ws.port=${WSP} --ws.origins="*" --ws.api="eth,debug,net,admin,web3,personal,txpool,ftm,sfc" \
+	--http --http.addr="127.0.0.1" --http.port=${RPCP} --http.corsdomain="*" --http.api="eth,debug,net,admin,web3,personal,txpool,ftm,dag" \
+	--ws --ws.addr="127.0.0.1" --ws.port=${WSP} --ws.origins="*" --ws.api="eth,debug,net,admin,web3,personal,txpool,ftm,dag" \
 	--nousb --verbosity=3 --tracing &> opera$i.log)&
 
     echo -e "\tnode$i ok"
