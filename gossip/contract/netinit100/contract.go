4d97f8747f25120ca2d7c681fb1850464a639886
alex <rusdev.alex@gmail.com>
2021-03-16 15:46:06 +1000

    rebase and update

---
+++ gossip/contract/netinit100/contract.go
@@ -0,0 +1,212 @@
+// Code generated - DO NOT EDIT.
+// This file is a generated binding and any manual changes will be lost.
+
+package netinit100
+
+import (
+	"math/big"
+	"strings"
+
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
+)
+
+// Reference imports to suppress errors if they are not otherwise used.
+var (
+	_ = big.NewInt
+	_ = strings.NewReader
+	_ = ethereum.NotFound
+	_ = bind.Bind
+	_ = common.Big1
+	_ = types.BloomLookup
+	_ = event.NewSubscription
+)
+
+// ContractABI is the input ABI used to generate the binding from.
+const ContractABI = "[{\"constant\":false,\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"sealedEpoch\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"totalSupply\",\"type\":\"uint256\"},{\"internalType\":\"address\",\"name\":\"_sfc\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"_auth\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"_driver\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"_evmWriter\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"_owner\",\"type\":\"address\"}],\"name\":\"initializeAll\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"
+
+// ContractBin is the compiled bytecode used for deploying new contracts.
+var ContractBin = "0x608060405234801561001057600080fd5b50610296806100206000396000f3fe608060405234801561001057600080fd5b506004361061002b5760003560e01c8063c80e151314610030575b600080fd5b610091600480360360e081101561004657600080fd5b5080359060208101359073ffffffffffffffffffffffffffffffffffffffff60408201358116916060810135821691608082013581169160a081013582169160c09091013516610093565b005b604080517f485cc95500000000000000000000000000000000000000000000000000000000815273ffffffffffffffffffffffffffffffffffffffff8681166004830152848116602483015291519185169163485cc9559160448082019260009290919082900301818387803b15801561010c57600080fd5b505af1158015610120573d6000803e3d6000fd5b5050604080517fc0c53b8b00000000000000000000000000000000000000000000000000000000815273ffffffffffffffffffffffffffffffffffffffff8981166004830152878116602483015285811660448301529151918816935063c0c53b8b925060648082019260009290919082900301818387803b1580156101a557600080fd5b505af11580156101b9573d6000803e3d6000fd5b5050604080517f019e2729000000000000000000000000000000000000000000000000000000008152600481018b9052602481018a905273ffffffffffffffffffffffffffffffffffffffff888116604483015285811660648301529151918916935063019e2729925060848082019260009290919082900301818387803b15801561024457600080fd5b505af1158015610258573d6000803e3d6000fd5b50600092505050fffea265627a7a723158208e09e31b87c09dafd92327a4a97a230838603e3ffc87a470e766320873a8e1a564736f6c634300050c0032"
+
+// DeployContract deploys a new Ethereum contract, binding an instance of Contract to it.
+func DeployContract(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *Contract, error) {
+	parsed, err := abi.JSON(strings.NewReader(ContractABI))
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+
+	address, tx, contract, err := bind.DeployContract(auth, parsed, common.FromHex(ContractBin), backend)
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	return address, tx, &Contract{ContractCaller: ContractCaller{contract: contract}, ContractTransactor: ContractTransactor{contract: contract}, ContractFilterer: ContractFilterer{contract: contract}}, nil
+}
+
+// Contract is an auto generated Go binding around an Ethereum contract.
+type Contract struct {
+	ContractCaller     // Read-only binding to the contract
+	ContractTransactor // Write-only binding to the contract
+	ContractFilterer   // Log filterer for contract events
+}
+
+// ContractCaller is an auto generated read-only Go binding around an Ethereum contract.
+type ContractCaller struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// ContractTransactor is an auto generated write-only Go binding around an Ethereum contract.
+type ContractTransactor struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// ContractFilterer is an auto generated log filtering Go binding around an Ethereum contract events.
+type ContractFilterer struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// ContractSession is an auto generated Go binding around an Ethereum contract,
+// with pre-set call and transact options.
+type ContractSession struct {
+	Contract     *Contract         // Generic contract binding to set the session for
+	CallOpts     bind.CallOpts     // Call options to use throughout this session
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// ContractCallerSession is an auto generated read-only Go binding around an Ethereum contract,
+// with pre-set call options.
+type ContractCallerSession struct {
+	Contract *ContractCaller // Generic contract caller binding to set the session for
+	CallOpts bind.CallOpts   // Call options to use throughout this session
+}
+
+// ContractTransactorSession is an auto generated write-only Go binding around an Ethereum contract,
+// with pre-set transact options.
+type ContractTransactorSession struct {
+	Contract     *ContractTransactor // Generic contract transactor binding to set the session for
+	TransactOpts bind.TransactOpts   // Transaction auth options to use throughout this session
+}
+
+// ContractRaw is an auto generated low-level Go binding around an Ethereum contract.
+type ContractRaw struct {
+	Contract *Contract // Generic contract binding to access the raw methods on
+}
+
+// ContractCallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
+type ContractCallerRaw struct {
+	Contract *ContractCaller // Generic read-only contract binding to access the raw methods on
+}
+
+// ContractTransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
+type ContractTransactorRaw struct {
+	Contract *ContractTransactor // Generic write-only contract binding to access the raw methods on
+}
+
+// NewContract creates a new instance of Contract, bound to a specific deployed contract.
+func NewContract(address common.Address, backend bind.ContractBackend) (*Contract, error) {
+	contract, err := bindContract(address, backend, backend, backend)
+	if err != nil {
+		return nil, err
+	}
+	return &Contract{ContractCaller: ContractCaller{contract: contract}, ContractTransactor: ContractTransactor{contract: contract}, ContractFilterer: ContractFilterer{contract: contract}}, nil
+}
+
+// NewContractCaller creates a new read-only instance of Contract, bound to a specific deployed contract.
+func NewContractCaller(address common.Address, caller bind.ContractCaller) (*ContractCaller, error) {
+	contract, err := bindContract(address, caller, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &ContractCaller{contract: contract}, nil
+}
+
+// NewContractTransactor creates a new write-only instance of Contract, bound to a specific deployed contract.
+func NewContractTransactor(address common.Address, transactor bind.ContractTransactor) (*ContractTransactor, error) {
+	contract, err := bindContract(address, nil, transactor, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &ContractTransactor{contract: contract}, nil
+}
+
+// NewContractFilterer creates a new log filterer instance of Contract, bound to a specific deployed contract.
+func NewContractFilterer(address common.Address, filterer bind.ContractFilterer) (*ContractFilterer, error) {
+	contract, err := bindContract(address, nil, nil, filterer)
+	if err != nil {
+		return nil, err
+	}
+	return &ContractFilterer{contract: contract}, nil
+}
+
+// bindContract binds a generic wrapper to an already deployed contract.
+func bindContract(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
+	parsed, err := abi.JSON(strings.NewReader(ContractABI))
+	if err != nil {
+		return nil, err
+	}
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Contract *ContractRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
+	return _Contract.Contract.ContractCaller.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Contract *ContractRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Contract.Contract.ContractTransactor.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Contract *ContractRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Contract.Contract.ContractTransactor.contract.Transact(opts, method, params...)
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Contract *ContractCallerRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
+	return _Contract.Contract.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Contract *ContractTransactorRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Contract.Contract.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Contract *ContractTransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Contract.Contract.contract.Transact(opts, method, params...)
+}
+
+// InitializeAll is a paid mutator transaction binding the contract method 0xc80e1513.
+//
+// Solidity: function initializeAll(uint256 sealedEpoch, uint256 totalSupply, address _sfc, address _auth, address _driver, address _evmWriter, address _owner) returns()
+func (_Contract *ContractTransactor) InitializeAll(opts *bind.TransactOpts, sealedEpoch *big.Int, totalSupply *big.Int, _sfc common.Address, _auth common.Address, _driver common.Address, _evmWriter common.Address, _owner common.Address) (*types.Transaction, error) {
+	return _Contract.contract.Transact(opts, "initializeAll", sealedEpoch, totalSupply, _sfc, _auth, _driver, _evmWriter, _owner)
+}
+
+// InitializeAll is a paid mutator transaction binding the contract method 0xc80e1513.
+//
+// Solidity: function initializeAll(uint256 sealedEpoch, uint256 totalSupply, address _sfc, address _auth, address _driver, address _evmWriter, address _owner) returns()
+func (_Contract *ContractSession) InitializeAll(sealedEpoch *big.Int, totalSupply *big.Int, _sfc common.Address, _auth common.Address, _driver common.Address, _evmWriter common.Address, _owner common.Address) (*types.Transaction, error) {
+	return _Contract.Contract.InitializeAll(&_Contract.TransactOpts, sealedEpoch, totalSupply, _sfc, _auth, _driver, _evmWriter, _owner)
+}
+
+// InitializeAll is a paid mutator transaction binding the contract method 0xc80e1513.
+//
+// Solidity: function initializeAll(uint256 sealedEpoch, uint256 totalSupply, address _sfc, address _auth, address _driver, address _evmWriter, address _owner) returns()
+func (_Contract *ContractTransactorSession) InitializeAll(sealedEpoch *big.Int, totalSupply *big.Int, _sfc common.Address, _auth common.Address, _driver common.Address, _evmWriter common.Address, _owner common.Address) (*types.Transaction, error) {
+	return _Contract.Contract.InitializeAll(&_Contract.TransactOpts, sealedEpoch, totalSupply, _sfc, _auth, _driver, _evmWriter, _owner)
+}
+
+var ContractBinRuntime = "0x608060405234801561001057600080fd5b506004361061002b5760003560e01c8063c80e151314610030575b600080fd5b610091600480360360e081101561004657600080fd5b5080359060208101359073ffffffffffffffffffffffffffffffffffffffff60408201358116916060810135821691608082013581169160a081013582169160c09091013516610093565b005b604080517f485cc95500000000000000000000000000000000000000000000000000000000815273ffffffffffffffffffffffffffffffffffffffff8681166004830152848116602483015291519185169163485cc9559160448082019260009290919082900301818387803b15801561010c57600080fd5b505af1158015610120573d6000803e3d6000fd5b5050604080517fc0c53b8b00000000000000000000000000000000000000000000000000000000815273ffffffffffffffffffffffffffffffffffffffff8981166004830152878116602483015285811660448301529151918816935063c0c53b8b925060648082019260009290919082900301818387803b1580156101a557600080fd5b505af11580156101b9573d6000803e3d6000fd5b5050604080517f019e2729000000000000000000000000000000000000000000000000000000008152600481018b9052602481018a905273ffffffffffffffffffffffffffffffffffffffff888116604483015285811660648301529151918916935063019e2729925060848082019260009290919082900301818387803b15801561024457600080fd5b505af1158015610258573d6000803e3d6000fd5b50600092505050fffea265627a7a723158208e09e31b87c09dafd92327a4a97a230838603e3ffc87a470e766320873a8e1a564736f6c634300050c0032"
--- gossip/sfc_test.go
+++ gossip/sfc_test.go
@@ -1,16 +1,24 @@
 package gossip
 
 // compile SFC with truffle
-//go:generate bash -c "cd ../../opera-sfc && git checkout 082a393d325a7523cc364e6db21359ab1b680904 && docker run --rm -v $(pwd)/../go-opera/gossip/contract/solc:/src/build/contracts -v $(pwd):/src -w /src node:10.5.0 bash -c 'export NPM_CONFIG_PREFIX=~; npm install --no-save; npm install --no-save truffle@5.1.4; npm run build'"
+//go:generate bash -c "cd ../../opera-sfc && git checkout 2aba3feb7ef932e85f6d1127fd07f451b83d7266 && docker run --rm -v $(pwd)/../go-opera/gossip/contract/solc:/src/build/contracts -v $(pwd):/src -w /src node:10.5.0 bash -c 'export NPM_CONFIG_PREFIX=~; npm install --no-save; npm install --no-save truffle@5.1.4; npm run build'"
 //go:generate bash -c "cd ./contract/solc && for f in *.json; do jq -j .bytecode $DOLLAR{f} > $DOLLAR{f%.json}.bin; jq -j .deployedBytecode $DOLLAR{f} > $DOLLAR{f%.json}.bin-runtime; jq -c .abi $DOLLAR{f} > $DOLLAR{f%.json}.abi; done"
-// wrap SFC with golang
+// wrap LegacySfcWrapper with golang
 //go:generate mkdir -p ./contract/sfc100
-//go:generate go run github.com/ethereum/go-ethereum/cmd/abigen --bin=./contract/solc/SFC.bin --abi=./contract/solc/SFC.abi --pkg=sfc100 --type=Contract --out=contract/sfc100/contract.go
-//go:generate bash -c "(echo -ne '\nvar ContractBinRuntime = \"'; cat contract/solc/SFC.bin-runtime; echo '\"') >> contract/sfc100/contract.go"
-// wrap Proxy with golang
-//go:generate mkdir -p ./contract/sfcproxy
-//go:generate go run github.com/ethereum/go-ethereum/cmd/abigen --bin=./contract/solc/Migrations.bin --abi=./contract/solc/Migrations.abi --pkg=sfcproxy --type=Contract --out=contract/sfcproxy/contract.go
-//go:generate bash -c "(echo -ne '\nvar ContractBinRuntime = \"'; cat contract/solc/Migrations.bin-runtime; echo '\"') >> contract/sfcproxy/contract.go"
+//go:generate go run github.com/ethereum/go-ethereum/cmd/abigen --bin=./contract/solc/LegacySfcWrapper.bin --abi=./contract/solc/LegacySfcWrapper.abi --pkg=sfc100 --type=Contract --out=contract/sfc100/contract.go
+//go:generate bash -c "(echo -ne '\nvar ContractBinRuntime = \"'; cat contract/solc/LegacySfcWrapper.bin-runtime; echo '\"') >> contract/sfc100/contract.go"
+// wrap NetworkInitializer with golang
+//go:generate mkdir -p ./contract/netinit100
+//go:generate go run github.com/ethereum/go-ethereum/cmd/abigen --bin=./contract/solc/NetworkInitializer.bin --abi=./contract/solc/NetworkInitializer.abi --pkg=netinit100 --type=Contract --out=contract/netinit100/contract.go
+//go:generate bash -c "(echo -ne '\nvar ContractBinRuntime = \"'; cat contract/solc/NetworkInitializer.bin-runtime; echo '\"') >> contract/netinit100/contract.go"
+// wrap NodeDriver with golang
+//go:generate mkdir -p ./contract/driver100
+//go:generate go run github.com/ethereum/go-ethereum/cmd/abigen --bin=./contract/solc/NodeDriver.bin --abi=./contract/solc/NodeDriver.abi --pkg=driver100 --type=Contract --out=contract/driver100/contract.go
+//go:generate bash -c "(echo -ne '\nvar ContractBinRuntime = \"'; cat contract/solc/NodeDriver.bin-runtime; echo '\"') >> contract/driver100/contract.go"
+// wrap NodeDriverAuth with golang
+//go:generate mkdir -p ./contract/driverauth100
+//go:generate go run github.com/ethereum/go-ethereum/cmd/abigen --bin=./contract/solc/NodeDriverAuth.bin --abi=./contract/solc/NodeDriverAuth.abi --pkg=driverauth100 --type=Contract --out=contract/driverauth100/contract.go
+//go:generate bash -c "(echo -ne '\nvar ContractBinRuntime = \"'; cat contract/solc/NodeDriverAuth.bin-runtime; echo '\"') >> contract/driverauth100/contract.go"
 
 import (
 	"fmt"
@@ -21,8 +29,10 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/stretchr/testify/require"
 
+	"github.com/Fantom-foundation/go-opera/gossip/contract/driver100"
+	"github.com/Fantom-foundation/go-opera/gossip/contract/driverauth100"
+	"github.com/Fantom-foundation/go-opera/gossip/contract/netinit100"
 	"github.com/Fantom-foundation/go-opera/gossip/contract/sfc100"
-	"github.com/Fantom-foundation/go-opera/gossip/contract/sfcproxy"
 	"github.com/Fantom-foundation/go-opera/logger"
 	"github.com/Fantom-foundation/go-opera/opera/genesis/driver"
 	"github.com/Fantom-foundation/go-opera/opera/genesis/driverauth"
@@ -38,12 +48,14 @@ func TestSFC(t *testing.T) {
 	env := newTestEnv()
 	defer env.Close()
 
-	sfcProxy, err := sfcproxy.NewContract(sfc.ContractAddress, env)
-	require.NoError(t, err)
+	/*
+		sfcProxy, err := sfcproxy.NewContract(sfc.ContractAddress, env)
+		require.NoError(t, err)
 
-	var (
-		sfc10 *sfc100.Contract
-	)
+		var (
+			sfc10 *sfc100.Contract
+		)
+	*/
 
 	_ = true &&
 
@@ -54,31 +66,7 @@ func TestSFC(t *testing.T) {
 			got, err := env.CodeAt(nil, sfc.ContractAddress, nil)
 			require.NoError(err)
 			require.Equal(exp, got, "genesis SFC contract")
-
-			require.Equal(len(exp), len(hexutil.MustDecode(sfc100.ContractBinRuntime)), "genesis SFC contract (lenght)")
-			require.Equal(exp, hexutil.MustDecode(sfc100.ContractBinRuntime), "genesis SFC contract")
-		}) &&
-
-		t.Run("Genesis DriverAuth", func(t *testing.T) {
-			require := require.New(t)
-
-			exp := driverauth.GetContractBin()
-			got, err := env.CodeAt(nil, driverauth.ContractAddress, nil)
-			require.NoError(err)
-			require.Equal(exp, got, "genesis DriverAuth contract")
-
-			// TODO: compare with hexutil.MustDecode( .ContractBinRuntime) also
-		}) &&
-
-		t.Run("Genesis Driver", func(t *testing.T) {
-			require := require.New(t)
-
-			exp := driver.GetContractBin()
-			got, err := env.CodeAt(nil, driver.ContractAddress, nil)
-			require.NoError(err)
-			require.Equal(exp, got, "genesis Driver contract")
-
-			// TODO: compare with hexutil.MustDecode( .ContractBinRuntime) also
+			require.Equal(exp, hexutil.MustDecode(sfc100.ContractBinRuntime), "genesis SFC contract version")
 		}) &&
 
 		t.Run("Network initializer", func(t *testing.T) {
@@ -89,11 +77,32 @@ func TestSFC(t *testing.T) {
 			require.NoError(err)
 			require.NotEmpty(exp, "genesis NetworkInitializer contract")
 			require.Empty(got, "genesis NetworkInitializer should be destructed")
+			require.Equal(exp, hexutil.MustDecode(netinit100.ContractBinRuntime), "genesis NetworkInitializer contract version")
+		}) &&
+
+		t.Run("Genesis Driver", func(t *testing.T) {
+			require := require.New(t)
+
+			exp := driver.GetContractBin()
+			got, err := env.CodeAt(nil, driver.ContractAddress, nil)
+			require.NoError(err)
+			require.Equal(exp, got, "genesis Driver contract")
+			require.Equal(exp, hexutil.MustDecode(driver100.ContractBinRuntime), "genesis Driver contract version")
+		}) &&
+
+		t.Run("Genesis DriverAuth", func(t *testing.T) {
+			require := require.New(t)
+
+			exp := driverauth.GetContractBin()
+			got, err := env.CodeAt(nil, driverauth.ContractAddress, nil)
+			require.NoError(err)
+			require.Equal(exp, got, "genesis DriverAuth contract")
+			require.Equal(exp, hexutil.MustDecode(driverauth100.ContractBinRuntime), "genesis DriverAuth contract version")
 		}) &&
 
 		t.Run("Some transfers I", func(t *testing.T) {
 			cicleTransfers(t, env, 1)
-		}) &&
+		}) /* &&
 
 		// TODO: up to v1.0.0
 		t.Run("Upgrade to develop", func(t *testing.T) {
@@ -120,7 +129,7 @@ func TestSFC(t *testing.T) {
 			epoch, err := sfc10.ContractCaller.CurrentEpoch(env.ReadOnly())
 			require.NoError(err)
 			require.Equal(0, epoch.Cmp(big.NewInt(3)), "current epoch %s", epoch.String())
-		})
+		})*/
 }
 
 func cicleTransfers(t *testing.T, env *testEnv, count uint64) {
