53e0c14811b88a1e2fc758afe178f669f0422d3a
alex <rusdev.alex@gmail.com>
2021-05-05 01:45:03 +1000

    new inter/Block.NotSkippedTxs()

--- inter/block.go
+++ inter/block.go
@@ -21,6 +21,25 @@ func (b *Block) EstimateSize() int {
 	return (len(b.Events)+len(b.InternalTxs)+len(b.Txs)+1+1)*32 + len(b.SkippedTxs)*4 + 8 + 8
 }
 
+func (b *Block) NotSkippedTxs() []common.Hash {
+	txs := append(b.InternalTxs, b.Txs...)
+
+	if len(b.SkippedTxs) == 0 {
+		// short circuit if nothing to skip
+		return txs
+	}
+	skipCount := 0
+	res := make([]common.Hash, 0, len(txs))
+	for i, tx := range txs {
+		if skipCount < len(b.SkippedTxs) && b.SkippedTxs[skipCount] == uint32(i) {
+			skipCount++
+		} else {
+			res = append(res, tx)
+		}
+	}
+	return res
+}
+
 func FilterSkippedTxs(txs types.Transactions, skippedTxs []uint32) types.Transactions {
 	if len(skippedTxs) == 0 {
 		// short circuit if nothing to skip
