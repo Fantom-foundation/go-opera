82dc120cbd585c4b60f77fc0867788aba746be5c
Egor Lysenko <uprendis@gmail.com>
2021-12-06 16:24:11 +0700

    optimize LLR emitting

--- gossip/emitter/emitter_llr.go
+++ gossip/emitter/emitter_llr.go
@@ -1,18 +1,23 @@
 package emitter
 
 import (
+	"math/rand"
+
 	"github.com/Fantom-foundation/lachesis-base/hash"
 	"github.com/Fantom-foundation/lachesis-base/inter/idx"
 
 	"github.com/Fantom-foundation/go-opera/eventcheck/basiccheck"
 	"github.com/Fantom-foundation/go-opera/inter"
+	"github.com/Fantom-foundation/go-opera/utils/piecefunc"
 )
 
+var emptyLlrBlockVotes = inter.LlrBlockVotes{
+	Votes: []hash.Hash{},
+}
+
 func (em *Emitter) addLlrBlockVotes(e *inter.MutableEventPayload) {
-	if e.Version() == 0 {
-		e.SetBlockVotes(inter.LlrBlockVotes{
-			Votes: []hash.Hash{},
-		})
+	if em.skipLlrBlockVote() || e.Version() == 0 {
+		e.SetBlockVotes(emptyLlrBlockVotes)
 		return
 	}
 	start := em.world.GetLowestBlockToDecide()
@@ -25,6 +30,7 @@ func (em *Emitter) addLlrBlockVotes(e *inter.MutableEventPayload) {
 		start = *prevInFile + 1
 	}
 	records := make([]hash.Hash, 0, 16)
+	epochEnd := false
 	var epoch idx.Epoch
 	for b := start; len(records) < basiccheck.MaxBlockVotesPerEvent; b++ {
 		record := em.world.GetBlockRecordHash(b)
@@ -36,13 +42,16 @@ func (em *Emitter) addLlrBlockVotes(e *inter.MutableEventPayload) {
 			epoch = blockEpoch
 		}
 		if epoch != blockEpoch || blockEpoch == 0 {
+			epochEnd = true
 			break
 		}
 		records = append(records, *record)
 	}
-	if len(records) == 0 {
-		start = 0
-		epoch = 0
+
+	waitUntilLongerBatch := !epochEnd && len(records) < basiccheck.MaxBlockVotesPerEvent
+	if len(records) == 0 || waitUntilLongerBatch {
+		e.SetBlockVotes(emptyLlrBlockVotes)
+		return
 	}
 	e.SetBlockVotes(inter.LlrBlockVotes{
 		Start: start,
@@ -52,7 +61,7 @@ func (em *Emitter) addLlrBlockVotes(e *inter.MutableEventPayload) {
 }
 
 func (em *Emitter) addLlrEpochVote(e *inter.MutableEventPayload) {
-	if e.Version() == 0 {
+	if em.skipLlrEpochVote() || e.Version() == 0 {
 		return
 	}
 	target := em.world.GetLowestEpochToDecide()
@@ -72,4 +81,28 @@ func (em *Emitter) addLlrEpochVote(e *inter.MutableEventPayload) {
 		Epoch: target,
 		Vote:  *vote,
 	})
+}
+
+func (em *Emitter) neverSkipLlrVote() bool {
+	return em.stakeRatio[em.config.Validator.ID] <= uint64(piecefunc.DecimalUnit)/3+1
+}
+
+func (em *Emitter) skipLlrBlockVote() bool {
+	if em.neverSkipLlrVote() {
+		return false
+	}
+	// poor validators vote only if we have a long batch of non-decided blocks
+	return em.world.GetLatestBlockIndex() < em.world.GetLowestBlockToDecide()+basiccheck.MaxBlockVotesPerEvent*3
+}
+
+func (em *Emitter) skipLlrEpochVote() bool {
+	if em.neverSkipLlrVote() {
+		return false
+	}
+	// poor validators vote if we have a long batch of non-decided epochs
+	if em.epoch > em.world.GetLowestEpochToDecide()+2 {
+		return false
+	}
+	// otherwise, poor validators have a small chance to vote
+	return rand.Intn(30) != 0
 }
