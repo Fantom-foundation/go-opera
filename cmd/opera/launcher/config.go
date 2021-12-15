24d373ea766af7cc0b62be72dcf73a3ea385b950
Egor Lysenko <uprendis@gmail.com>
2021-12-15 14:29:45 +0700

    fix ignoring bootnodes config/flag for non-fakenet networks

--- cmd/opera/launcher/config.go
+++ cmd/opera/launcher/config.go
@@ -317,9 +317,6 @@ func gossipStoreConfigWithFlags(ctx *cli.Context, src gossip.StoreConfig) (gossi
 func nodeConfigWithFlags(ctx *cli.Context, cfg node.Config) node.Config {
 	utils.SetNodeConfig(ctx, &cfg)
 
-	if !ctx.GlobalIsSet(FakeNetFlag.Name) {
-		setBootnodes(ctx, Bootnodes, &cfg)
-	}
 	setDataDir(ctx, &cfg)
 	return cfg
 }
@@ -357,6 +354,8 @@ func mayMakeAllConfigs(ctx *cli.Context) (*config, error) {
 	if ctx.GlobalIsSet(FakeNetFlag.Name) {
 		_, num, _ := parseFakeGen(ctx.GlobalString(FakeNetFlag.Name))
 		cfg.Opera = gossip.FakeConfig(num, cacheRatio)
+	} else {
+		setBootnodes(ctx, Bootnodes, &cfg.Node)
 	}
 
 	// Load config file (medium priority)
