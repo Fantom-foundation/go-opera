b3276c3b62498627be69cc66326aceb0ecc51098
alex <rusdev.alex@gmail.com>
2021-12-16 22:25:02 +1000

    demo: connect 2 peers

--- demo/_params.sh
+++ demo/_params.sh
@@ -6,3 +6,30 @@ declare -r  TAG="${TAG:-latest}"
 PORT_BASE=3000
 RPCP_BASE=4000
 WSP_BASE=4500
+
+attach_and_exec() {
+    local i=$1
+    local CMD=$2
+    local RPCP=$(($RPCP_BASE+$i))
+
+    for attempt in $(seq 40)
+    do
+        if (( attempt > 5 ))
+        then
+            echo "  - attempt ${attempt}: " >&2
+        fi
+
+        res=$(../build/demo_opera --exec "${CMD}" attach http://127.0.0.1:${RPCP} 2> /dev/null)
+        if [ $? -eq 0 ]
+        then
+            #echo "success" >&2
+            echo $res
+            return 0
+        else
+            #echo "wait" >&2
+            sleep 1
+        fi
+    done
+    echo "failed RPC connection to ${NAME}" >&2
+    return 1
+}
--- demo/start.sh
+++ demo/start.sh
@@ -30,43 +30,18 @@ do
     echo -e "\tnode$i ok"
 done
 
-attach_and_exec() {
-    local i=$1
-    local CMD=$2
-    local RPCP=$(($RPCP_BASE+$i))
-
-    for attempt in $(seq 40)
-    do
-        if (( attempt > 5 ));
-        then 
-            echo "  - attempt ${attempt}: " >&2
-        fi;
-
-        res=$(../build/demo_opera --exec "${CMD}" attach http://127.0.0.1:${RPCP} 2> /dev/null)
-        if [ $? -eq 0 ]
-        then
-            #echo "success" >&2
-            echo $res
-            return 0
-        else
-            #echo "wait" >&2
-            sleep 1
-        fi
-    done
-    echo "failed RPC connection to ${NAME}" >&2
-    return 1
-}
-
-
 echo -e "\nConnect nodes to ring:\n"
 for ((i=0;i<$N;i+=1))
 do
-    j=$(((i+1) % N))
+    for n in 1 2
+    do
+        j=$(((i+n) % N))
 
-    enode=$(attach_and_exec $j 'admin.nodeInfo.enode')
-    echo "    p2p address = ${enode}"
+	enode=$(attach_and_exec $j 'admin.nodeInfo.enode')
+        echo "    p2p address = ${enode}"
 
-    echo " connecting node-$i to node-$j:"
-    res=$(attach_and_exec $i "admin.addPeer(${enode})")
-    echo "    result = ${res}"
+        echo " connecting node-$i to node-$j:"
+        res=$(attach_and_exec $i "admin.addPeer(${enode})")
+        echo "    result = ${res}"
+    done
 done
