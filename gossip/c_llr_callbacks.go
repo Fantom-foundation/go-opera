07cfb8d4d66c92ddac0a4b894628c543dccf16eb
Egor Lysenko <uprendis@gmail.com>
2022-01-27 07:23:47 +0700

    fix flushing without engineMu being locked

--- gossip/c_llr_callbacks.go
+++ gossip/c_llr_callbacks.go
@@ -79,11 +79,12 @@ func (s *Service) processBlockVotes(bvs inter.LlrSignedBlockVotes) error {
 }
 
 func (s *Service) ProcessBlockVotes(bvs inter.LlrSignedBlockVotes) error {
+	// engineMu should be locked here
 	defer s.mayCommit(false)
 	return s.processBlockVotes(bvs)
 }
 
-func (s *Service) processFullBlockRecord(br ibr.LlrIdxFullBlockRecord) error {
+func (s *Service) ProcessFullBlockRecord(br ibr.LlrIdxFullBlockRecord) error {
 	// engineMu should NOT be locked here
 	if s.store.HasBlock(br.Idx) {
 		return eventcheck.ErrAlreadyProcessedBR
@@ -143,13 +144,9 @@ func (s *Service) processFullBlockRecord(br ibr.LlrIdxFullBlockRecord) error {
 		}
 	}
 	updateLowestBlockToFill(br.Idx, s.store)
+	s.mayCommit(false)
 
 	return nil
-}
-
-func (s *Service) ProcessFullBlockRecord(br ibr.LlrIdxFullBlockRecord) error {
-	defer s.mayCommit(false)
-	return s.processFullBlockRecord(br)
 }
 
 func (s *Service) processRawEpochVote(epoch idx.Epoch, ev hash.Hash, val idx.Validator, vals *pos.Validators, llrs *LlrState) {
@@ -201,11 +198,12 @@ func (s *Service) processEpochVote(ev inter.LlrSignedEpochVote) error {
 }
 
 func (s *Service) ProcessEpochVote(ev inter.LlrSignedEpochVote) error {
+	// engineMu should be locked here
 	defer s.mayCommit(false)
 	return s.processEpochVote(ev)
 }
 
-func (s *Service) processFullEpochRecord(er ier.LlrIdxFullEpochRecord) error {
+func (s *Service) ProcessFullEpochRecord(er ier.LlrIdxFullEpochRecord) error {
 	// engineMu should NOT be locked here
 	if s.store.HasHistoryBlockEpochState(er.Idx) {
 		return eventcheck.ErrAlreadyProcessedER
@@ -226,13 +224,9 @@ func (s *Service) processFullEpochRecord(er ier.LlrIdxFullEpochRecord) error {
 	s.engineMu.Lock()
 	defer s.engineMu.Unlock()
 	updateLowestEpochToFill(er.Idx, s.store)
+	s.mayCommit(false)
 
 	return nil
-}
-
-func (s *Service) ProcessFullEpochRecord(er ier.LlrIdxFullEpochRecord) error {
-	defer s.mayCommit(false)
-	return s.processFullEpochRecord(er)
 }
 
 func updateLowestBlockToFill(block idx.Block, store *Store) {
