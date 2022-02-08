cc4fa7b56a1940cd128ffe2ba4c0ea4763782054
alex <rusdev.alex@gmail.com>
2022-02-08 20:46:51 +1000

    txtrace test from release/txtracing/1.0.2-rc.8

---
+++ gossip/txtrace_test.go
@@ -0,0 +1,79 @@
+package gossip
+
+// Called contract
+//go:generate bash -c "docker run --rm -v $(pwd)/contract/called:/src -v $(pwd)/contract:/dst ethereum/solc:0.8.4 -o /dst/solc/ --optimize --optimize-runs=200 --bin --abi --allow-paths /src --overwrite /src/Called.sol"
+//go:generate bash -c "go run github.com/ethereum/go-ethereum/cmd/abigen --bin=./contract/solc/Called.bin --abi=./contract/solc/Called.abi --pkg=called --type=Contract --out=./contract/called/contract.go"
+
+// Caller contract
+//go:generate bash -c "docker run --rm -v $(pwd)/contract/caller:/src -v $(pwd)/contract:/dst ethereum/solc:0.8.4 -o /dst/solc/ --optimize --optimize-runs=200 --bin --abi --allow-paths /src --overwrite /src/Caller.sol"
+//go:generate bash -c "go run github.com/ethereum/go-ethereum/cmd/abigen --bin=./contract/solc/Caller.bin --abi=./contract/solc/Caller.abi --pkg=caller --type=Contract --out=./contract/caller/contract.go"
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/Fantom-foundation/go-opera/gossip/contract/called"
+	"github.com/Fantom-foundation/go-opera/gossip/contract/caller"
+	"github.com/Fantom-foundation/go-opera/logger"
+)
+
+func TestTxTracing(t *testing.T) {
+	logger.SetTestMode(t)
+
+	env := newTestEnv()
+	defer env.Close()
+	backend := &EthAPIBackend{state: env.stateReader}
+
+	var (
+		contract *caller.Contract
+		library  *called.Contract
+	)
+	const (
+		admin = 1
+		key   = 9
+	)
+
+	addr, tx, library, err := called.DeployContract(env.Payer(admin), env)
+	require.NoError(t, err)
+	require.NotNil(t, library)
+	env.ApplyBlock(time.Second, tx)
+	env.incNonce(env.Address(admin))
+
+	_, tx, contract, err = caller.DeployContract(env.Payer(admin), env, addr)
+	require.NoError(t, err)
+	require.NotNil(t, contract)
+	env.ApplyBlock(time.Second, tx)
+	env.incNonce(env.Address(admin))
+
+	t.Run("SubReverts", func(t *testing.T) {
+		require := require.New(t)
+
+		tx, err := contract.Inc(env.Payer(admin), key)
+		require.NoError(err)
+		receipts := env.ApplyBlock(time.Second, tx)
+		env.incNonce(env.Address(admin))
+		require.NotEmpty(receipts)
+
+		actions, err := backend.TxTraceByHash(context.Background(), tx.Hash())
+		require.NoError(err)
+		require.NotNil(actions)
+		require.Len(*actions, 3)
+
+		reverted := 0
+		for _, action := range *actions {
+			if action.Error == "Reverted" {
+				reverted += 1
+			}
+		}
+		require.Equal(1, reverted)
+
+		// visulization
+		traceStr, err := json.Marshal(actions)
+		require.NoError(err)
+		t.Logf("traces: %s", string(traceStr))
+	})
+}
