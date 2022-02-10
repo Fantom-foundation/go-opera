37081c3d5480eb09a429ab470a7010a6795044cc
Egor Lysenko <uprendis@gmail.com>
2022-02-10 15:06:03 +0700

    add filehash testcases with different content and piece sizes

--- opera/genesisstore/fileshash/filehash_test.go
+++ opera/genesisstore/fileshash/filehash_test.go
@@ -1,28 +1,18 @@
 package fileshash
 
 import (
+	"bytes"
 	"io"
 	"io/ioutil"
+	"math"
+	"math/rand"
 	"os"
 	"testing"
 
 	"github.com/Fantom-foundation/lachesis-base/hash"
 	"github.com/stretchr/testify/require"
-)
 
-const (
-	FILE_HASH    = "0x15c45aba675b7c49f5def32b4f24e827d478e5dfd712613fd6a5df69a2793b60"
-	FILE_CONTENT = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-			Nunc finibus ultricies interdum. Nulla porttitor arcu a tincidunt mollis. Aliquam erat volutpat. 
-			Maecenas eget ligula mi. Maecenas in ligula non elit fringilla consequat. 
-			Morbi non imperdiet odio. Integer viverra ligula a varius tempor. 
-			Duis ac velit vel augue faucibus tincidunt ut ac nisl. Nulla sed magna est. 
-			Etiam quis nunc in elit ultricies pulvinar sed at felis. 
-			Suspendisse fringilla lectus vel est hendrerit pulvinar. 
-			Vivamus nec lorem pharetra ligula pulvinar blandit in quis nunc. 
-			Cras id eros fermentum mauris tristique faucibus. 
-			Praesent vehicula lectus nec ipsum sollicitudin tempus. Nullam et massa velit.`
-	PIECE_SIZE = 100
+	"github.com/Fantom-foundation/go-opera/utils/ioread"
 )
 
 type dropableFile struct {
@@ -36,6 +26,52 @@ func (f dropableFile) Drop() error {
 }
 
 func TestFileHash_ReadWrite(t *testing.T) {
+
+	const (
+		FILE_CONTENT = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+			Nunc finibus ultricies interdum. Nulla porttitor arcu a tincidunt mollis. Aliquam erat volutpat. 
+			Maecenas eget ligula mi. Maecenas in ligula non elit fringilla consequat. 
+			Morbi non imperdiet odio. Integer viverra ligula a varius tempor. 
+			Duis ac velit vel augue faucibus tincidunt ut ac nisl. Nulla sed magna est. 
+			Etiam quis nunc in elit ultricies pulvinar sed at felis. 
+			Suspendisse fringilla lectus vel est hendrerit pulvinar. 
+			Vivamus nec lorem pharetra ligula pulvinar blandit in quis nunc. 
+			Cras id eros fermentum mauris tristique faucibus. 
+			Praesent vehicula lectus nec ipsum sollicitudin tempus. Nullam et massa velit.`
+	)
+	t.Run("Large file, pieceSize=1", func(t *testing.T) {
+		testFileHash_ReadWrite(t, bytes.Repeat([]byte(FILE_CONTENT), 20), hash.HexToHash("0x433c09b0d4b79c1eb49dfa9030da39cd992112b2732d0214e8927acbe5edc447"), 1)
+	})
+	t.Run("Large file, pieceSize=2", func(t *testing.T) {
+		testFileHash_ReadWrite(t, bytes.Repeat([]byte(FILE_CONTENT), 20), hash.HexToHash("0xbbe4e85a32695e42945d66b3d5c6b0201419163870011142332d124c2be387c6"), 2)
+	})
+	t.Run("Large file, pieceSize=100", func(t *testing.T) {
+		testFileHash_ReadWrite(t, bytes.Repeat([]byte(FILE_CONTENT), 20), hash.HexToHash("0xdc6d882fde82b2dd44884a97884d79be40e1d0f780a493dee0f7256d8261f7a5"), 100)
+	})
+	t.Run("Medium file, pieceSize=1", func(t *testing.T) {
+		testFileHash_ReadWrite(t, bytes.Repeat([]byte(FILE_CONTENT), 1), hash.HexToHash("0x63a76929ee27decd5100d07a3cb626c05df1f1e927c5f27fa17c62459685ca6f"), 1)
+	})
+	t.Run("Medium file, pieceSize=2", func(t *testing.T) {
+		testFileHash_ReadWrite(t, bytes.Repeat([]byte(FILE_CONTENT), 1), hash.HexToHash("0x2babd1049c449a60da62a1a0a3dc836e6a222dece07dcba1890a041d60ff29c7"), 2)
+	})
+	t.Run("Medium file, pieceSize=100", func(t *testing.T) {
+		testFileHash_ReadWrite(t, bytes.Repeat([]byte(FILE_CONTENT), 1), hash.HexToHash("0x15c45aba675b7c49f5def32b4f24e827d478e5dfd712613fd6a5df69a2793b60"), 100)
+	})
+	t.Run("Tiny file, pieceSize=1", func(t *testing.T) {
+		testFileHash_ReadWrite(t, []byte{0}, hash.HexToHash("0xbdda25ac486f2b8c0330a6fcace8f3d05d3e713b7920a39a1f60c0d8df024c0e"), 1)
+	})
+	t.Run("Tiny file, pieceSize=2", func(t *testing.T) {
+		testFileHash_ReadWrite(t, []byte{0}, hash.HexToHash("0xf2b22424f7d1d01467d650f18ad49df8929fbefdeefe95e868d52eec6ea399e1"), 2)
+	})
+	t.Run("Empty file, pieceSize=1", func(t *testing.T) {
+		testFileHash_ReadWrite(t, []byte{}, hash.HexToHash("0x9cbc73d18d70c94fe366e696035c4f2cffdbab7ea6d6c2c039ca185f9c9f2746"), 1)
+	})
+	t.Run("Empty file, pieceSize=2", func(t *testing.T) {
+		testFileHash_ReadWrite(t, []byte{}, hash.HexToHash("0x163e7f66d58036ccb1d0b0058d8f46e7cd639816f570e5eb32853ea73634e4cd"), 2)
+	})
+}
+
+func testFileHash_ReadWrite(t *testing.T, content []byte, expRoot hash.Hash, pieceSize uint64) {
 	require := require.New(t)
 	tmpDirPath, err := ioutil.TempDir("", "filehash*")
 	defer os.RemoveAll(tmpDirPath)
@@ -43,7 +79,7 @@ func TestFileHash_ReadWrite(t *testing.T) {
 	f, err := ioutil.TempFile(tmpDirPath, "testnet.g")
 	filePath := f.Name()
 	require.NoError(err)
-	writer := WrapWriter(f, PIECE_SIZE, 200, func() TmpWriter {
+	writer := WrapWriter(f, pieceSize, 200, func() TmpWriter {
 		tmpFh, err := ioutil.TempFile(tmpDirPath, "genesis.*.dat")
 		require.NoError(err)
 		return dropableFile{
@@ -54,23 +90,36 @@ func TestFileHash_ReadWrite(t *testing.T) {
 	})
 
 	// write out the (secure) self-hashed file properly
-	_, err = writer.Write([]byte(FILE_CONTENT))
+	_, err = writer.Write(content)
 	require.NoError(err)
 	root, err := writer.Flush()
 	require.NoError(err)
-	require.Equal(root.Hex(), FILE_HASH)
+	require.Equal(expRoot.Hex(), root.Hex())
 	f.Close()
 
-	// normal case: correct root hash and content
+	maxMemUsage := memUsageOf(pieceSize, getPiecesNum(uint64(len(content)), pieceSize))
+
+	// normal case: correct root hash and content after reading file partially
+	if len(content) > 0 {
+		f, err = os.OpenFile(filePath, os.O_RDONLY, 0600)
+		require.NoError(err)
+		reader := WrapReader(f, maxMemUsage, root)
+		readB := make([]byte, rand.Int63n(int64(len(content))))
+		err = ioread.ReadAll(reader, readB)
+		require.NoError(err)
+		require.Equal(content[:len(readB)], readB)
+		reader.Close()
+	}
+
+	// normal case: correct root hash and content after reading the whole file
 	{
 		f, err = os.OpenFile(filePath, os.O_RDONLY, 0600)
 		require.NoError(err)
-		reader := WrapReader(f, 2048, root)
-		data := make([]byte, 5)
-		n, err := reader.Read(data)
-		require.Equal("Lorem", string(data[:]))
+		reader := WrapReader(f, maxMemUsage, root)
+		readB := make([]byte, len(content))
+		err = ioread.ReadAll(reader, readB)
 		require.NoError(err)
-		require.Equal(n, len(data))
+		require.Equal(content, readB)
 		reader.Close()
 	}
 
@@ -78,54 +127,84 @@ func TestFileHash_ReadWrite(t *testing.T) {
 	{
 		f, err = os.OpenFile(filePath, os.O_RDONLY, 0600)
 		require.NoError(err)
-		maliciousReader := WrapReader(f, 2048, hash.HexToHash("0x00"))
-		data := make([]byte, PIECE_SIZE)
-		_, err = maliciousReader.Read(data)
-		require.ErrorIs(err, ErrRootMismatch)
+		maliciousReader := WrapReader(f, maxMemUsage, hash.HexToHash("0x00"))
+		data := make([]byte, 1)
+		err = ioread.ReadAll(maliciousReader, data)
+		require.Contains(err.Error(), ErrInit.Error())
 		maliciousReader.Close()
 	}
 
 	// modify piece data to make the mismatch piece hash
-	{
-		f, err = os.OpenFile(filePath, os.O_WRONLY, 0600)
+	headerOffset := 4 + 8 + getPiecesNum(uint64(len(content)), pieceSize)*32
+	if len(content) > 0 {
+		// mutate content byte
+		f, err = os.OpenFile(filePath, os.O_RDWR, 0600)
 		require.NoError(err)
-		f.Seek(300, 0)
-		s := []byte("foobar")
-		f.Write(s)
-		f.Close()
+		s := []byte{0}
+		contentPos := rand.Int63n(int64(len(content)))
+		pos := int64(headerOffset) + contentPos
+		_, err = f.ReadAt(s, pos)
+		require.NoError(err)
+		s[0]++
+		_, err = f.WriteAt(s, pos)
+		require.NoError(err)
+		require.NoError(f.Close())
 
+		// try to read
 		f, err = os.OpenFile(filePath, os.O_RDONLY, 0600)
-		maliciousReader := WrapReader(f, 2048, root)
-		data := make([]byte, PIECE_SIZE)
-		_, err = maliciousReader.Read(data)
+		maliciousReader := WrapReader(f, maxMemUsage, root)
+		data := make([]byte, contentPos+1)
+		err = ioread.ReadAll(maliciousReader, data)
 		require.ErrorIs(err, ErrHashMismatch)
-		maliciousReader.Close()
+		require.NoError(maliciousReader.Close())
+
+		// restore
+		s[0]--
+		f, err = os.OpenFile(filePath, os.O_RDWR, 0600)
+		require.NoError(err)
+		_, err = f.WriteAt(s, pos)
+		require.NoError(err)
+		require.NoError(f.Close())
 	}
 
 	// modify a piece hash in file to make the wrong one
 	{
-		f, err = os.OpenFile(filePath, os.O_WRONLY, 0600)
+		// mutate content byte
+		f, err = os.OpenFile(filePath, os.O_RDWR, 0600)
 		require.NoError(err)
-		f.Seek(16, 0)
-		s := []byte("0000000000")
-		f.Write(s)
-		f.Close()
+		pos := rand.Int63n(int64(headerOffset))
+		s := []byte{0}
+		_, err = f.ReadAt(s, pos)
+		require.NoError(err)
+		s[0]++
+		_, err = f.WriteAt(s, pos)
+		require.NoError(err)
+		require.NoError(f.Close())
 
+		// try to read
 		f, err = os.OpenFile(filePath, os.O_RDONLY, 0600)
-		maliciousReader := WrapReader(f, 2048, root)
-		data := make([]byte, PIECE_SIZE)
-		_, err = maliciousReader.Read(data)
-		require.ErrorIs(err, ErrRootMismatch)
-		maliciousReader.Close()
+		maliciousReader := WrapReader(f, math.MaxUint64, root)
+		data := make([]byte, 1)
+		err = ioread.ReadAll(maliciousReader, data)
+		require.Contains(err.Error(), ErrInit.Error())
+		require.NoError(maliciousReader.Close())
+
+		// restore
+		s[0]--
+		f, err = os.OpenFile(filePath, os.O_RDWR, 0600)
+		require.NoError(err)
+		_, err = f.WriteAt(s, pos)
+		require.NoError(err)
+		require.NoError(f.Close())
 	}
 
 	// hashed file requires too much memory
 	{
 		f, err = os.OpenFile(filePath, os.O_WRONLY, 0600)
 		require.NoError(err)
-		oomReader := WrapReader(f, 16, root)
-		data := make([]byte, PIECE_SIZE)
-		_, err = oomReader.Read(data)
+		oomReader := WrapReader(f, maxMemUsage-1, root)
+		data := make([]byte, 1)
+		err = ioread.ReadAll(oomReader, data)
 		require.Errorf(err, "hashed file requires too much memory")
 	}
 }
