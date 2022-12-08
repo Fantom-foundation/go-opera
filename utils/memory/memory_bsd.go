d3019d913be75cb81ec54d91916e26d880dc46cc
Egor Lysenko <uprendis@gmail.com>
2022-12-08 19:54:36 +0400

    add memory package from https://github.com/pbnjay/memory

---
+++ utils/memory/memory_bsd.go
@@ -0,0 +1,20 @@
+//go:build freebsd || openbsd || dragonfly || netbsd
+// +build freebsd openbsd dragonfly netbsd
+
+package memory
+
+func sysTotalMemory() uint64 {
+	s, err := sysctlUint64("hw.physmem")
+	if err != nil {
+		return 0
+	}
+	return s
+}
+
+func sysFreeMemory() uint64 {
+	s, err := sysctlUint64("hw.usermem")
+	if err != nil {
+		return 0
+	}
+	return s
+}
---
+++ utils/memory/stub.go
@@ -0,0 +1,11 @@
+//go:build !linux && !darwin && !windows && !freebsd && !dragonfly && !netbsd && !openbsd
+// +build !linux,!darwin,!windows,!freebsd,!dragonfly,!netbsd,!openbsd
+
+package memory
+
+func sysTotalMemory() uint64 {
+	return 0
+}
+func sysFreeMemory() uint64 {
+	return 0
+}
