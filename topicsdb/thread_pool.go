143cf986ca18eb34972c5e34e63c0c50c6d08008
alex <rusdev.alex@gmail.com>
2023-01-10 01:26:56 +1000

    WithThreadPool.ForEachInBlocks() uses global thread pool

--- topicsdb/thread_pool.go
+++ topicsdb/thread_pool.go
@@ -2,22 +2,52 @@ package topicsdb
 
 import (
 	"context"
-	"fmt"
 	"runtime/debug"
+	"sync"
+	"time"
 
 	"github.com/Fantom-foundation/lachesis-base/inter/idx"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 )
 
-type WithThreadPool struct {
-	*Index
+type pool struct {
+	mu    sync.Mutex
+	sum   int
+	queue []int
+}
+
+func (p *pool) Lock(want int) (got int, release func()) {
+	if want < 1 {
+		want = 0
+	}
+
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	got = min(p.sum, want)
+	p.sum -= got
+	release = func() {
+		p.mu.Lock()
+		defer p.mu.Unlock()
+		p.sum += got
+	}
+
+	return
+}
+
+var globalPool = &pool{
+	sum: getMaxThreads(),
 }
 
 func getMaxThreads() int {
 	was := debug.SetMaxThreads(10000)
 	debug.SetMaxThreads(was)
 	return was
+}
+
+type WithThreadPool struct {
+	*Index
 }
 
 // FindInBlocks returns all log records of block range by pattern. 1st pattern element is an address.
@@ -50,8 +80,6 @@ func (tt *WithThreadPool) ForEachInBlocks(ctx context.Context, from, to idx.Bloc
 		return err
 	}
 
-	fmt.Printf("getMaxThreads() == %d\n", getMaxThreads())
-
 	onMatched := func(rec *logrec) (gonext bool, err error) {
 		rec.fetch(tt.table.Logrec)
 		if rec.err != nil {
@@ -62,5 +90,40 @@ func (tt *WithThreadPool) ForEachInBlocks(ctx context.Context, from, to idx.Bloc
 		return
 	}
 
-	return tt.searchParallel(ctx, pattern, uint64(from), uint64(to), onMatched)
+	splitby := 0
+	threads := 0
+	for i := range pattern {
+		threads += len(pattern[i])
+		if len(pattern[splitby]) < len(pattern[i]) {
+			splitby = i
+		}
+	}
+	rest := pattern[splitby]
+	threads -= len(rest)
+
+	for len(rest) > 0 {
+		got, release := globalPool.Lock(threads + len(rest))
+		if got <= threads {
+			release()
+			time.Sleep(time.Microsecond)
+			continue
+		}
+
+		pattern[splitby] = rest[:got-threads]
+		rest = rest[got-threads:]
+		err = tt.searchParallel(ctx, pattern, uint64(from), uint64(to), onMatched)
+		release()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
