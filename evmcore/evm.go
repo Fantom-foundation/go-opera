36d09b77ea802f339ce9712f790a0ddca8f1767c
Maxim Prishchepo <dev@sfxdx.com>
2023-04-14 15:14:10 +0300

    Fix TxContext field types

--- evmcore/evm.go
+++ evmcore/evm.go
@@ -63,8 +63,8 @@ func NewEVMTxContext(msg Message) vm.TxContext {
 	return vm.TxContext{
 		Origin:             msg.From(),
 		GasPrice:           new(big.Int).Set(msg.GasPrice()),
-		GasLimit:           new(big.Int).SetUint64(msg.Gas()),
-		Nonce:              new(big.Int).SetUint64(msg.Nonce()),
+		GasLimit:           msg.Gas(),
+		Nonce:              msg.Nonce(),
 		TransactionFeePaid: !msg.IsAA(),
 	}
 }
