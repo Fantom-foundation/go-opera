bbe9e8633d850eb714f0b6a0f2164df052a523b8
Shevchuk <es@grapherex.com>
2023-10-04 19:00:55 +0700

    simple store setter test

--- gossip/evmstore/store_test.go
+++ gossip/evmstore/store_test.go
@@ -1,6 +1,11 @@
 package evmstore
 
 import (
+	"testing"
+
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/stretchr/testify/assert"
+
 	"github.com/Fantom-foundation/lachesis-base/kvdb/memorydb"
 )
 
@@ -14,4 +19,15 @@ func nonCachedStore() *Store {
 	cfg := StoreConfig{}
 
 	return NewStore(memorydb.NewProducer(""), cfg)
+}
+
+func TestStoreSetTx(t *testing.T) {
+	store := cachedStore()
+
+	tx := types.NewTx(&types.LegacyTx{Data: []byte("test")})
+
+	store.SetTx(tx.Hash(), tx)
+
+	txFromStore := store.GetTx(tx.Hash())
+	assert.Equal(t, tx.Data(), txFromStore.Data())
 }
